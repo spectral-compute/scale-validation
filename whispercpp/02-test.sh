278c572ac8e07ea7e493d161242cdbd4e29eee4f
Chris Kitching <chriskitching@linux.com>
2025-04-25 02:46:46 +0200

    Fix silly scripting issue in whisper.cpp test

--- whispercpp/02-test.sh
+++ whispercpp/02-test.sh
@@ -4,9 +4,9 @@ set -e
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
-cd "${OUT_DIR}/whispercpp/whispercpp/build"
+cd "${OUT_DIR}/whispercpp/whispercpp"
 
 set -o pipefail
-./build/bin/main -f samples/jfk.wav | tee output.txt
+make base.en -j10 | tee output.txt
 
 cat output.txt | grep "And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country."
