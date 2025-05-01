872470b0a6e228d153e0fca4a019965155f77381
Nicholas Tomlinson <nstomlinson@gmail.com>
2025-05-01 01:24:55 +0100

    Fix the GROMACS benchMEM test

--- gromacs/02-benchMEM_FAILS.sh
+++ gromacs/02-benchMEM_FAILS.sh
@@ -7,7 +7,9 @@ export LD_LIBRARY_PATH="${CUDA_DIR}/lib"
 # Download the benchmark data if it doesn't already exist.
 mkdir -p "${OUT_DIR}/data/MaxPlanckInstituteGromacsBenchmarks"
 cd "${OUT_DIR}/data/MaxPlanckInstituteGromacsBenchmarks"
-wget -q https://www.mpinat.mpg.de/benchMEM.zip
+if [ ! -e benchMEM.zip ] ; then
+    wget -q https://www.mpinat.mpg.de/benchMEM.zip
+fi
 
 # Create somewhere for results.
 RESULT_FILE="${OUT_DIR}/gromacs/$(basename -s .sh "$0").csv"
@@ -15,9 +17,11 @@ rm -f "${RESULT_FILE}"
 
 RESULT_DIR="${OUT_DIR}/gromacs/benchmarks/MaxPlanckInstitute"
 mkdir -p "${RESULT_DIR}"
+if [ ! -e benchMEM.tpr ] ; then
+    unzip "${OUT_DIR}/data/MaxPlanckInstituteGromacsBenchmarks/benchMEM.zip"
+fi
 
 source "${OUT_DIR}/gromacs/install/bin/GMXRC"
-unzip benchMEM.zip
 #gmx mdrun -s 
 gmx mdrun -s benchMEM.tpr -nb gpu 
 echo "Done"
