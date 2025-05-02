41ff32dd0439c94e41a0b4a0768fcf86014f5e99
Nicholas Tomlinson <nstomlinson@gmail.com>
2025-05-02 02:06:49 +0100

    Make sure GROMACS doesn't regress
    And notice if it progresses.

--- gromacs/02-gromacs-regression-test.sh
+++ gromacs/02-gromacs-regression-test.sh
@@ -6,9 +6,24 @@ cd "${OUT_DIR}/gromacs/regressiontests-2024.4"
 
 source "${OUT_DIR}/gromacs/install/bin/GMXRC"
 
-set +e
-./gmxtest.pl all
+OUT="${OUT_DIR}/gromacs/regression-test.txt"
 
-if [ "$?" != "0" ] ; then
-    exit 222
+set +e
+./gmxtest.pl all 2>&1 | tee "${OUT}"
+
+# Unfortunately, not all of the tests pass (even on Nvidia). So we check the number of PASSEDs and Abnormals.
+PASSED="$(grep -F PASSED "${OUT}" | wc -l)"
+ABNORMAL="$(grep -F Abnormal "${OUT}" | wc -l)"
+
+echo "Number of passes: ${PASSED}"
+echo "Number of abnormals: ${ABNORMAL}"
+
+if [ "${PASSED}" != "132" ] ; then
+    echo "Incorrect number of passing tests!"
+    exit 1
 fi
+if [ "${ABNORMAL}" != "14" ] ; then
+    echo "Incorrect number of abnormal tests!"
+    exit 1
+fi
+echo "GROMACS regression tests performed as expected :)"
