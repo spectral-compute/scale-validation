0b08b0f2df31e8971c6685b589964d1abf776f7b
finlay <fgch500@york.ac.uk>
2025-05-06 18:18:06 +0100

    bump llamacpp tag version

--- llama.cpp/00-clone.sh
+++ llama.cpp/00-clone.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")"/../util/args.sh "$@"
 
 mkdir -p "${OUT_DIR}/llama.cpp"
 cd "${OUT_DIR}/llama.cpp"
-do_clone llama.cpp https://github.com/ggerganov/llama.cpp b1500
+do_clone llama.cpp https://github.com/ggerganov/llama.cpp b2000
 
 mkdir models
 cd models
