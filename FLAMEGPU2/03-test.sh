fea66afb2dc96ba0e22bc296acc42ef831dd65c5
Chris Kitching <chriskitching@linux.com>
2025-05-11 20:35:20 +0200

    Disable FLAMEGPU tests affected by the gfx9* AQL bug

--- FLAMEGPU2/03-test.sh
+++ FLAMEGPU2/03-test.sh
@@ -10,4 +10,15 @@ export FLAMEGPU_INC_DIR="${OUT_DIR}/FLAMEGPU2/FLAMEGPU2/include"
 # Err why is this needed only in CI?
 export CUDA_PATH="${CUDA_PATH}"
 
-./bin/Release/tests --gtest_filter='-LoggingTest.CUDAEnsembleSimulate:DependencyGraphTest.UnattachedFunctionWarning'
+FILTERS='-LoggingTest.CUDAEnsembleSimulate:DependencyGraphTest.UnattachedFunctionWarning'
+
+# On gfx9xx, a bug in the AQL queue implementation causes intermittent deadlocks when multiple
+# compute queues are used. Sighhhh.
+# These tests are testing for actual speedups from using stream concurrency for kernels, which is
+# disabled in SCALE on affected AMD architectures until we finish migrating away from AQL to
+# something that works (but is, regrettably, undocumented).
+if [[ "$SCALE_ENV" == gfx9* ]]; then
+  FILTERS="$FILTERS:TestCUDASimulationConcurrency*"
+fi
+
+./bin/Release/tests --gtest_filter=$FILTERS
