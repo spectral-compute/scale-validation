00b16cba3dc9ca97a93fb8f57a663fa2cb8b1c34
Chris Kitching <chriskitching@linux.com>
2025-05-11 18:04:07 +0200

    Remove inaccurate comment
    We actually fixed and re-enabled the context lifecyle tests a
    little while ago.

--- FLAMEGPU2/03-test.sh
+++ FLAMEGPU2/03-test.sh
@@ -10,5 +10,4 @@ export FLAMEGPU_INC_DIR="${OUT_DIR}/FLAMEGPU2/FLAMEGPU2/include"
 # Err why is this needed only in CI?
 export CUDA_PATH="${CUDA_PATH}"
 
-# Our context support isn't *quite* matching yet.
 ./bin/Release/tests --gtest_filter='-LoggingTest.CUDAEnsembleSimulate:DependencyGraphTest.UnattachedFunctionWarning'
