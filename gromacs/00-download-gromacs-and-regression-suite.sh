8117cdad18d37effedb721a9c9f16edb48696176
Chris Kitching <chriskitching@linux.com>
2025-05-24 13:37:18 +0200

    Only apply gromacs patch where it's needed

--- gromacs/00-download-gromacs-and-regression-suite.sh
+++ gromacs/00-download-gromacs-and-regression-suite.sh
@@ -14,10 +14,12 @@ cd "${OUT_DIR}/gromacs"
 
 do_clone gromacs https://github.com/gromacs/gromacs.git v${GROMACS_VER}
 
-# Fix a few upstream bugs to make non-MPI gromacs builds actually build.
-# (If you build a cuda-aware MPI using the scripts here, you should be able to
-# hook it up to GROMACS, too!).
-git -C "${OUT_DIR}/gromacs/gromacs" apply "${SCRIPT_DIR}/gromacs_patches.diff"
+if [ ${GROMACS_VER} = "2025.1" ]; then
+  # Fix a few upstream bugs in 2025.1 to make non-MPI gromacs builds actually build.
+  # (If you build a cuda-aware MPI using the scripts here, you should be able to
+  # hook it up to GROMACS, too!).
+  git -C "${OUT_DIR}/gromacs/gromacs" apply "${SCRIPT_DIR}/gromacs_patches.diff"
+fi
 
 wget -q https://ftp.gromacs.org/regressiontests/regressiontests-${GROMACS_VER}.tar.gz
 tar xf regressiontests-${GROMACS_VER}.tar.gz
