79c5d9b227eb9cfdd69c034624837e087657d6f4
Chris Kitching <chriskitching@linux.com>
2025-08-14 15:58:21 +0200

    Disbale tensormaps explicitly in cutlass

--- cutlass/01-patch.sh
+++ cutlass/01-patch.sh
@@ -13,6 +13,12 @@ sed -Ee 's|#  define CUTLASS_SM90_CLUSTER_LAUNCH_ENABLED|//CUTLASS_SM90_CLUSTER_
 # Disable sparse mma, because we haven't implemented it yet.
 sed -Ee 's|#define CUTLASS_ARCH_SPARSE_MMA_SM80_ENABLED|//CUTLASS_ARCH_SPARSE_MMA_SM80_ENABLED|' -i ${SRCDIR}/include/cutlass/arch/mma_sparse_sm80.h
 
+# Disable tensormaps, because we haven't implemented them yet.
+sed -Ee 's|#define CUDA_HOST_ADAPTER_TENSORMAP_ENABLED|//CUDA_HOST_ADAPTER_TENSORMAP_ENABLED|' -i ${SRCDIR}/include/cutlass/cuda_host_adapter.hpp
+
+# Disable TMA, because we haven't implemented it yet.
+sed -Ee 's|# *define *CUTE_COPY_ATOM_TMA_SM90_ENABLED|//CUTE_COPY_ATOM_TMA_SM90_ENABLED|' -i ${SRCDIR}/include/cute/atom/copy_atom.hpp
+
 # Disable fp8, because we haven't implemented it yet.
 sed -Ee 's|#define CUDA_FP8_ENABLED|//CUDA_FP8_ENABLED|' -i ${SRCDIR}/include/cutlass/float8.h
 
