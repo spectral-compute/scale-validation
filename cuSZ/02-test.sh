00ed096a520486f6e8081a138ee921695cbd06d7
Chris Kitching <chriskitching@linux.com>
2025-08-22 16:08:03 +0200

    cuSZ works now

--- cuSZ/02-test.sh
+++ cuSZ/02-test.sh
@@ -8,10 +8,10 @@ export LD_LIBRARY_PATH="${CUDA_PATH}/lib"
 for F in $(find . -maxdepth 1 -type f -perm /u+x | grep -vE '\.so$' | sort) ; do
     echo -e "\x1b[1m${F}\x1b[m"
     case "${F}" in
-        ./l1_compact|./l2_cudaproto)
+        ./l1_compact)
             echo -e '\x1b[31;1mCrashes on Nvidia\x1b[m'
         ;;
-        ./lrz_seq|./spv_cu)
+        ./spv_cu)
             "${F}" || true
             echo -e '\x1b[33;1mBroken on Nvidia\x1b[m'
         ;;
