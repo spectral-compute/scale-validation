7fc059afcf1df81eb764b398cca4cc777b258e7b
Nicholas Tomlinson <nstomlinson@gmail.com>
2025-09-03 19:39:01 +0100

    Improve CUTLASS test scripts

--- cutlass/03-test.sh
+++ cutlass/03-test.sh
@@ -5,4 +5,25 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
 cd "${OUT_DIR}/cutlass/build"
-for i in $(find test/unit -type f -executable); do echo "------------ TEST: $i"; $i; done
+
+TESTS=(
+    $(find test -type f -perm /100 | sort)
+)
+
+set +e
+FAILURES=()
+for T in "${TESTS[@]}" ; do
+    echo "======== ${T} ========"
+    "${T}"
+    if [ "$?" != "0" ] ; then
+        FAILURES+=("${T}")
+    fi
+done
+set -e
+
+for T in "${FAILURES[@]}" ; do
+    echo "Failed: ${T}"
+done
+if [ ! -z "${FAILURES}" ] ; then
+    exit 1
+fi
