c7cb29daefb72f2df5ce00ef0c3375cc83b6ca6c
Jon Chesterfield <jonathanchesterfield@gmail.com>
2025-09-12 14:19:37 +0100

    Redirect cutlass tests to a file and unroll the bash settings

--- cutlass/03-test.sh
+++ cutlass/03-test.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-set -ETeuo pipefail
+# set -ETeuo pipefail # Script previously started with this, unrolling them
+set -o errtrace
+set -o functrace
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
 cd "${OUT_DIR}/cutlass/build"
 
+LOGFILE=${OUT_DIR}/cutlass/build/tests.log
+
+echo "Writing to $LOGFILE"
+
 TESTS=(
-    $(find test -type f -perm /100 | sort)
+    $(find test -type f -executable | sort)
 )
 
 set +e
 FAILURES=()
 for T in "${TESTS[@]}" ; do
     echo "======== ${T} ========"
-    "${T}"
+    "${T}" >> $LOGFILE
     if [ "$?" != "0" ] ; then
         FAILURES+=("${T}")
     fi
