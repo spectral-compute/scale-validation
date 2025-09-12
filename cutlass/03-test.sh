1dc0e07a1ddbe20d3d492fe055afd8b0b00e81b1
Jon Chesterfield <jon@spectralcompute.co.uk>
2025-09-12 15:02:43 +0100

    Filter out some large smem tests and a first pass at summarising total pass/fail count

--- cutlass/03-test.sh
+++ cutlass/03-test.sh
@@ -10,18 +10,34 @@ source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
 cd "${OUT_DIR}/cutlass/build"
 
-LOGFILE=${OUT_DIR}/cutlass/build/tests.log
+LOGFILE="${OUT_DIR}/cutlass/build/tests.log"
 
 echo "Writing to $LOGFILE"
+rm -f "$LOGFILE"
+
+FILTERS='*:-'
+
+# These tests require > 64k of smem to execute
+for LargeSMEM in \
+    SM80_CuTe_Ampere.CooperativeGemm2_Double_MMA \
+    SM80_CuTe_Ampere.CooperativeGemm3_Half_MMA_CustomSmemLayouts \
+    SM80_CuTe_Ampere.CooperativeGemm4_Half_MMA_SwizzledSmemLayouts \
+    SM80_CuTe_Ampere.CooperativeGemm5_Double_MMA_SwizzledSmemLayouts \
+    SM80_CuTe_Ampere.CooperativeGemmComposedStride
+do    
+    FILTERS="$FILTERS:$LargeSMEM"
+done
 
 TESTS=(
     $(find test -type f -executable | sort)
 )
 
+# Currently this runs every test under the same negative filter list.
+
 FAILURES=()
 for T in "${TESTS[@]}" ; do
-    echo "======== ${T} ========"
-    "${T}" >> $LOGFILE
+    echo "======== ${T} ========" | tee -a $LOGFILE
+    "${T}" --gtest_filter=$FILTERS >> $LOGFILE
     if [ "$?" != "0" ] ; then
         FAILURES+=("${T}")
     fi
@@ -33,6 +49,13 @@ done
 if [ ! -z "${FAILURES}" ] ; then
     exit 1
 fi
+
+
+echo "Summary"
+# todo, might want to emit json / xml or similar instead of hacking it out with grep
+grep '\[  PASSED  \] [0-9]* tests[.]' "$LOGFILE" | cut -d ' ' -f 6 | paste -sd+ | bc
+grep '\[  FAILED  \] [0-9]* tests, listed below:' "$LOGFILE" | cut -d ' ' -f 6 | paste -sd+ | bc
+
 
 echo "Cutlass test script finished"
 exit 0
