0620ea150372317e71d41d124320efd18e2a3e4a
Jon Chesterfield <jon@spectralcompute.co.uk>
2025-09-16 18:28:51 +0100

    patch to build with nvcc

--- cutlass/01-patch.sh
+++ cutlass/01-patch.sh
@@ -7,32 +7,7 @@ source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
 SRCDIR="${OUT_DIR}/cutlass/cutlass"
 
-# Cutlass disables wmma with clang due to an old clang bug that doesn't apply to SCALE.
-#sed -Ee 's|#if !\(defined\(__clang__\) && defined\(__CUDA__\)\)|#if 1|' -i ${SRCDIR}/include/cutlass/arch/wmma.h
-#sed -Ee 's|#if !\(defined\(__clang__\) && defined\(__CUDA__\)\)|#if 1|' -i ${SRCDIR}/include/cutlass/epilogue/warp/fragment_iterator_wmma_tensor_op.h
-#sed -Ee 's|#if !\(defined\(__clang__\) && defined\(__CUDA__\)\)|#if 1|' -i ${SRCDIR}/include/cutlass/epilogue/warp/tile_iterator_wmma_tensor_op.h
-
-# Always disable clustering.
-sed -Ee 's|#  define CUTLASS_SM90_CLUSTER_LAUNCH_ENABLED|//CUTLASS_SM90_CLUSTER_LAUNCH_ENABLED|' -i ${SRCDIR}/include/cutlass/cluster_launch.hpp
-
-# Disable sparse mma, because we haven't implemented it yet.
-sed -Ee 's|#define CUTLASS_ARCH_SPARSE_MMA_SM80_ENABLED|//CUTLASS_ARCH_SPARSE_MMA_SM80_ENABLED|' -i ${SRCDIR}/include/cutlass/arch/mma_sparse_sm80.h
-
-# Disable tensormaps, because we haven't implemented them yet.
-sed -Ee 's|#define CUDA_HOST_ADAPTER_TENSORMAP_ENABLED|//CUDA_HOST_ADAPTER_TENSORMAP_ENABLED|' -i ${SRCDIR}/include/cutlass/cuda_host_adapter.hpp
-sed -Ee 's|#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90_SUPPORTED|//#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90_SUPPORTED|' -i ${SRCDIR}/include/cutlass/arch/config.h
-sed -Ee 's|#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90_ENABLED|//#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90_ENABLED|' -i ${SRCDIR}/include/cutlass/arch/config.h
-sed -Ee 's|#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90A_ENABLED|//#define CUTLASS_ARCH_MMA_MODIFIABLE_TMA_SM90A_ENABLED|' -i ${SRCDIR}/include/cutlass/arch/config.h
-
-# Disable TMA, because we haven't implemented it yet.
-sed -Ee 's|# *define *CUTE_COPY_ATOM_TMA_SM90_ENABLED|//CUTE_COPY_ATOM_TMA_SM90_ENABLED|' -i ${SRCDIR}/include/cute/atom/copy_atom.hpp
-sed -Ee 's|#define CUDA_12_0_SM90_FEATURES_SUPPORTED true|//#define CUDA_12_0_SM90_FEATURES_SUPPORTED true|' -i ${SRCDIR}/test/unit/common/cutlass_unit_test.h
-
-# Disable fp8, because we haven't implemented it yet.
-sed -Ee 's|#define CUDA_FP8_ENABLED|//CUDA_FP8_ENABLED|' -i ${SRCDIR}/include/cutlass/float8.h
-
-# Skip the sm90 example, since it uses tensormaps too.
-sed -Ee 's|wgmma_sm90.cu|sgemm_sm80.cu|' -i ${SRCDIR}/examples/cute/tutorial/CMakeLists.txt
-
-# Disable SM-number checking to skip tests, so all tests always run.
-sed -Ee 's|supported = false;|supported = true;|' -i ${SRCDIR}/examples/13_two_tensor_op_fusion/test_run.h
+# This could be done better, device properties doesn't have a clock rate any more
+sed -i 's$deviceProperties.clockRate$2300$g' "$SRCDIR"/test/unit/common/filter_architecture.cpp
+sed -i 's$properties\[0\].clockRate$2300$g' "$SRCDIR"/tools/profiler/src/options.cu
+sed -i 's$prop.clockRate$2300$g' "$SRCDIR"/tools/profiler/src/options.cu
--- cutlass/02-build.sh
+++ cutlass/02-build.sh
@@ -6,16 +6,29 @@ source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
 export SCALE_CUDA_VERSION="11.4"
 
+# getting
+#CMake Error: Error required internal CMake variable not set, cmake may not be built correctly.
+#Missing variable is:
+#_CMAKE_CUDA_WHOLE_FLAG
+# possible workaround is -std=c++17 -stdlib=libc++ -D_ALLOW_UNSUPPORTED_LIBCPP in CUDA_FLAGS
+# that gets us to   Clang device compilation for CUTLASS requires CMake 3.30 or higher.
+# installed 4.1.1 under /usr/local
+
+# trying to find nvidia::cudart with the prefix path
+#    -DCMAKE_CUDA_FLAGS="-Wno-unused-result -Wno-deprecated-declarations -std=c++17 -stdlib=libc++ -D_ALLOW_UNSUPPORTED_LIBCPP" \
+# /usr/local/cuda-13 has more stuff in it, e.g. cuda_runtime.h, than /usr/local/cuda
 # Configure.
-cmake \
+/usr/local/bin/cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu \
     -DCUTLASS_NVCC_ARCHS="$(echo "${GPU_ARCH}" | sed -E 's/sm_//g')" \
     -DCUTLASS_TEST_UNIT_ENABLE_WARNINGS=ON \
     -DCMAKE_CUDA_ARCHITECTURES="$(echo "${GPU_ARCH}" | sed -E 's/sm_//g')" \
-    -DCMAKE_CUDA_COMPILER="${CUDA_PATH}/bin/nvcc" \
-    -DCMAKE_CUDA_FLAGS="-Wno-unused-result -Wno-deprecated-declarations" \
-    -DCMAKE_CXX_COMPILER="${CUDA_PATH}/bin/clang++" \
-    -DCMAKE_C_COMPILER="${CUDA_PATH}/bin/clang" \
+    -DCMAKE_CUDA_COMPILER="/usr/local/cuda-13/bin/nvcc" \
+    -DCUDA_TOOLKIT_ROOT_DIR="/usr/local/cuda-13" \
+    -DCUDA_CUDA_FLAGS="-Wno-unused-result -Wno-deprecated-declarations" \
+    -DCMAKE_CXX_COMPILER="g++" \
+    -DCMAKE_C_COMPILER="gcc" \
     -B"${OUT_DIR}/cutlass/build" \
     "${OUT_DIR}/cutlass/cutlass"
 
