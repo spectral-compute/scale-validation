9df9728b701fabe847cf45bf4fe39f0a99e441e5
Chris Kitching <chriskitching@linux.com>
2025-10-18 16:46:54 +0200

    Add Stringzila test script

---
+++ StringZilla/01-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+source "${SCRIPT_DIR}"/../util/args.sh "$@"
+
+# Configure.
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER="${CUDA_PATH}/bin/clang" \
+    -DCMAKE_CXX_COMPILER="${CUDA_PATH}/bin/clang++" \
+    -DSTRINGZILLA_BUILD_TEST=1 \
+    -B"${OUT_DIR}/StringZilla/StringZilla/build" \
+    "${OUT_DIR}/StringZilla/StringZilla"
+
+make -C "${OUT_DIR}/StringZilla/StringZilla/build" -j"${BUILD_JOBS}"
