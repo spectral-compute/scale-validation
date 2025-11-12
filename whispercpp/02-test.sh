0627ae3f7f3cdb896649fb02e325d4d06e209b39
Jon Chesterfield <jon@spectralcompute.co.uk>
2025-11-12 04:36:38 +0000

    Revise whispercpp test runner to more explicitly call the gpu build

--- whispercpp/02-test.sh
+++ whispercpp/02-test.sh
@@ -7,6 +7,24 @@ source "${SCRIPT_DIR}"/../util/args.sh "$@"
 cd "${OUT_DIR}/whispercpp/whispercpp"
 
 set -o pipefail
-make base.en -j10 | tee output.txt
 
-cat output.txt | grep "And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country."
+# Do the moral equivalent of make base.en -j10 | tee output.txt
+
+# Download the base model
+bash ./models/download-ggml-model.sh base.en
+
+# Download the samples too
+make samples
+
+# Like the `make base.en target`, except run the sample on the GPU build
+# That is ./main is cpu-only, ./build/bin/main is the gpu build
+
+for f in samples/*.wav; do
+    echo "----------------------------------------------"
+    echo ""
+    ./build/bin/main -m models/ggml-base.en.bin -f "$f" &> "$f.out"
+    echo ""
+done
+
+
+cat samples/jfk.wav.out | grep "And so my fellow Americans, ask not what your country can do for you, ask what you can do for your country."
