299b1f63883e2fd1196af9af521f0872f270fa65
Manos <manos@spectralcompute.co.uk>
2025-12-03 13:28:58 +0000

    New pytorch version 2.9.0

---
+++ pytorch_2.9.0/02-copy-pytorch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+source "$(dirname "$0")"/../util/args.sh "$@"
+
+# We have to copy so the late stages of setup.py build work.
+rm -rf "${OUT_DIR}/pytorch/build"
+cp -r --reflink=auto "${OUT_DIR}/pytorch/pytorch" "${OUT_DIR}/pytorch/build"
+cd "${OUT_DIR}/pytorch/build"
