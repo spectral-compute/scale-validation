aefdffdaf15c763a26c61fa69ce2b030559e307b
Chris Kitching <chriskitching@linux.com>
2026-01-17 18:07:55 +0100

    Fix trivial GROMACS script bug

--- gromacs/00-download-gromacs.sh
+++ gromacs/00-download-gromacs.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-GROMACS_VER="$(get_version gromacs)"
-
 source "$(dirname "$0")"/../util/args.sh "$@"
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-source "${SCRIPT_DIR}"/../util/args.sh "$@"
+
+GROMACS_VER="$(get_version gromacs)"
 
 mkdir -p "${OUT_DIR}/gromacs"
 cd "${OUT_DIR}/gromacs"
 
-do_clone gromacs https://github.com/gromacs/gromacs.git v${GROMACS_VER}
+do_clone gromacs https://github.com/gromacs/gromacs.git ${GROMACS_VER}
 
 if [ ${GROMACS_VER} = "2025.1" ]; then
   # Fix a few upstream bugs in 2025.1 to make non-MPI gromacs builds actually build.
