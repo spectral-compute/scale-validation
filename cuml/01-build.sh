9498ae9aa2f8e1a9f3cbf9f4a567554e0eaae25e
Chris Kitching <chriskitching@linux.com>
2026-01-17 18:37:43 +0100

    Make cuml script less obviously incorrect

--- cuml/01-build.sh
+++ cuml/01-build.sh
@@ -4,8 +4,5 @@ set -ETeuo pipefail
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 source "${SCRIPT_DIR}"/../util/args.sh "$@"
 
-export PATH="${CUDA_PATH}/bin:${PATH}"
-export CUDACXX="${CUDA_PATH}/bin/nvcc"
-
-cd "${OUT_DIR}/cudf/cudf"
+cd "${OUT_DIR}/cuml/cuml"
 ./build.sh
