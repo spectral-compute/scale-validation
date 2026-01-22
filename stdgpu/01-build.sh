d0a9921543837c54e92b72c4165827b608ce58eb
Chris Kitching <chriskitching@linux.com>
2026-01-22 15:32:04 +0100

    Don't try to install stdgpu to root...

--- stdgpu/01-build.sh
+++ stdgpu/01-build.sh
@@ -7,7 +7,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CUDA_COMPILER="${CUDA_PATH}/bin/nvcc" \
     -DCMAKE_CUDA_ARCHITECTURES="${SCALE_FAKE_CUDA_ARCH}" \
-    -DCMAKE_INSTALL_PREFIX="${OUT_DIR}/stdgpu/install" \
+    -DCMAKE_INSTALL_PREFIX="install" \
     -B"build" \
     "stdgpu"
 
