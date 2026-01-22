6d53d59e12b2514619e8cae8338f4ca14ae77466
Chris Kitching <chriskitching@linux.com>
2026-01-22 13:31:47 +0100

    Fix GOMC pathing issues

--- gomc/01-build.sh
+++ gomc/01-build.sh
@@ -5,12 +5,12 @@ set -e
 # Compile for the architectures we want.
 GPU_ARCH="${SCALE_FAKE_CUDA_ARCH}"
 for F in GOMC/CMake/GOMCCUDASetup.cmake GOMC/test/BuildGPUTests.cmake ; do
-    sed -E "s/(\s+CUDA_ARCHITECTURES ).*/\1${GPU_ARCH}/" -i "GOMC/${F}"
+    sed -E "s/(\s+CUDA_ARCHITECTURES ).*/\1${GPU_ARCH}/" -i "${F}"
 done
 
 # CMake detects that we're Clang, and GOMC enables libc++ for that in a way that seems to be broken.
 for F in GOMC/CMakeLists.txt GOMC/lib/cereal-1.3.0/CMakeLists.txt ; do
-    sed -E 's/-stdlib=libc[+][+]//' -i "GOMC/${F}"
+    sed -E 's/-stdlib=libc[+][+]//' -i "${F}"
 done
 
 # GOMC's OpenMP support tests __GNUC__'s value. Even if I set this to a larger value that causes use of the newer OpenMP
@@ -32,4 +32,4 @@ cmake \
     -B"build" \
     "GOMC"
 
-make -C "$build" -j"$(nproc)"
+make -C "build" -j"$(nproc)"
--- gomc/03-example.sh
+++ gomc/03-example.sh
@@ -2,9 +2,10 @@
 
 set -ETeuo pipefail
 
+OUT_DIR=$(realpath .)
 cd "GOMC_Examples/NVT_GEMC/pure_fluid/octane_T_360_00_K"
 git clean -df .
-"../build/GOMC_GPU_GEMC" in.conf 2>&1 | tee test.log
+"${OUT_DIR}/build/GOMC_GPU_GEMC" in.conf 2>&1 | tee test.log
 
 if grep -F 'Warning: Updated energy differs from Recalculated Energy!' test.log ; then
     echo "Error: Result is incorrect."
