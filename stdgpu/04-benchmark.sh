95d8b809e5c7a85d9f25187140f6daf5892c1ecc
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-26 21:43:13 +0000

    stdgpu: WIP: faulty benchmarks not getting excluded?

--- stdgpu/04-benchmark.sh
+++ stdgpu/04-benchmark.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 set -e
-cd "${OUT_DIR}/stdgpu"
 
 # Skip faulty benchmarks. See scale#385.
-SKIP="
-stdgpu_unordered_map_insert/10000000
+SKIP="stdgpu_unordered_map_insert/10000000
 stdgpu_unordered_map_erase/10000000
 stdgpu_unordered_map_clear/10000000
 stdgpu_unordered_map_erase/100000
 stdgpu_unordered_set_insert/.*
 stdgpu_unordered_set_erase/.*
-stdgpu_unordered_set_clear/.*
-"
+stdgpu_unordered_set_clear/.*"
 
 SKIP=$(echo $SKIP | sed 's/ /|/g')
 
-build/bin/benchmarkstdgpu --benchmark_filter="-$SKIP" --benchmark_out_format=csv --benchmark_out=out.csv
+# TODO: I think this isn't excluding the above list properly?
+# ./build/bin/benchmarkstdgpu --benchmark_filter="(?!$SKIP)" --benchmark_out_format=csv --benchmark_out=out.csv
