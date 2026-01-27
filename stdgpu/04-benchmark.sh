44803525d50e74e7e8b9de8ee7a4846280929ff6
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-27 19:09:20 +0000

    stdgpu: re-enable benchmarks

--- stdgpu/04-benchmark.sh
+++ stdgpu/04-benchmark.sh
@@ -3,15 +3,10 @@
 set -e
 
 # Skip faulty benchmarks. See scale#385.
-SKIP="stdgpu_unordered_map_insert/10000000
-stdgpu_unordered_map_erase/10000000
-stdgpu_unordered_map_clear/10000000
-stdgpu_unordered_map_erase/100000
-stdgpu_unordered_set_insert/.*
-stdgpu_unordered_set_erase/.*
-stdgpu_unordered_set_clear/.*"
+SKIP="stdgpu_unordered_.*_insert/.*
+stdgpu_unordered_map_erase/.*
+stdgpu_unordered_map_clear/.*"
 
 SKIP=$(echo $SKIP | sed 's/ /|/g')
 
-# TODO: I think this isn't excluding the above list properly?
-# ./build/bin/benchmarkstdgpu --benchmark_filter="(?!$SKIP)" --benchmark_out_format=csv --benchmark_out=out.csv
+./build/bin/benchmarkstdgpu --benchmark_filter="-$SKIP" --benchmark_out_format=csv --benchmark_out=out.csv
