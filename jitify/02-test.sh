4e6b0f5eaec682387beab4b72a1f08e3ae1b29ff
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-28 11:08:38 +0000

    jitify: some tests are sensitive to being run in-tree

--- jitify/02-test.sh
+++ jitify/02-test.sh
@@ -2,5 +2,6 @@
 
 set -ETeuo pipefail
 
+cd jitify
 # TODO: https://gitlab.com/spectral-ai/engineering/cuda/platform/redscale/-/issues/549
-./jitify/jitify_test --gtest_filter="-JitifyTest.ConstantMemory:JitifyTest.ConstantMemory_experimental:JitifyTest.RemoveUnusedGlobals:JitifyTest.LinkExternalFiles:JitifyTest.LinkCurrentExecutable"
+./jitify_test --gtest_filter="-JitifyTest.ConstantMemory:JitifyTest.ConstantMemory_experimental:JitifyTest.RemoveUnusedGlobals:JitifyTest.LinkExternalFiles:JitifyTest.LinkCurrentExecutable"
