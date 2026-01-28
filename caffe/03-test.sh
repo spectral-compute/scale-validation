528ae99bebcf6d408241bf2df0ee629bd539ceb8
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-28 15:00:11 +0000

    caffe: fix typo in excluded tests spec

--- caffe/03-test.sh
+++ caffe/03-test.sh
@@ -3,5 +3,5 @@
 set -e
 
 set +e
-# The BenchmarkTest.* tests are overly fragile. This is a defect in those tests, not SCALE
-cd build && GTEST_FILTER='-BenchmarkTest.*' make runtest
+# The BenchmarkTest* tests are overly fragile. This is a defect in those tests, not SCALE
+cd build && GTEST_FILTER='-BenchmarkTest*' make runtest
