ef3a0156caf36968605ee6d37b6cc74714043f1a
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-01-28 10:56:13 +0000

    stdgpu: more typos in benchmark skipping >:(

--- stdgpu/04-benchmark.sh
+++ stdgpu/04-benchmark.sh
@@ -4,9 +4,9 @@ set -e
 
 # Skip faulty benchmarks. See scale#385.
 SKIP="stdgpu_unordered_.*_insert/.*
-stdgpu_unordered_map_erase/.*
-stdgpu_unordered_map_clear/.*"
+stdgpu_unordered_.*_erase/.*
+stdgpu_unordered_.*_clear/.*"
 
 SKIP=$(echo $SKIP | sed 's/ /|/g')
 
-./build/bin/benchmarkstdgpu --benchmark_filter="-$SKIP" --benchmark_out_format=csv --benchmark_out=out.csv
+./build/bin/benchmarkstdgpu --benchmark_filter="-$SKIP" --benchmark_out_format=csv --benchmark_out=out.csv --v=3
