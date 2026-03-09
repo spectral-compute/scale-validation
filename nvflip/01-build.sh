d2c59ce4155ac07dddd979b42ef284e87e05a70b
Nicholas Tomlinson <nstomlinson@gmail.com>
2026-03-09 13:32:36 +0000

    Don't force host compiler when building nvflip

--- nvflip/01-build.sh
+++ nvflip/01-build.sh
@@ -5,9 +5,6 @@ set -e
 # Configure.
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DCMAKE_CXX_FLAGS="-Wno-error=c++11-narrowing" \
     -DFLIP_ENABLE_CUDA=ON \
     -B"build" \
     "nvflip/src"
