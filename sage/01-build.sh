73a4818a8e583c7329941e6f82f984fe3e96c7ae
Chris Kitching <chriskitching@linux.com>
2026-03-13 07:24:42 +0100

    Self-host the whispercpp sample files
    Test kept failing for silly download reasons

---
+++ sage/01-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ETeuo pipefail
+
+# Configure.
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="-Wno-stringop-overread -Wno-format-truncation" \
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
+    -DCMAKE_CUDA_COMPILER="nvcc" \
+    -B"build" \
+    "alien"
+
+# Build.
+cmake --build "build" -j"$(nproc)"
