9751f6e61602a8585c427d05d3f1ed252faf9afa
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-04-13 13:41:51 +0100

    ggml: Honor NO_TUNE_NATIVE

--- ggml/01-build.sh
+++ ggml/01-build.sh
@@ -2,9 +2,15 @@
 
 set -ETeuo pipefail
 
+GGML_NATIVE=On
+if [ "${NO_TUNE_NATIVE:-0}" == "1" ]; then
+    GGML_NATIVE=Off
+fi
+
 cmake \
 	-DGGML_CUDA=ON \
 	-DCMAKE_INSTALL_PREFIX="install_ggml" \
+    -DGGML_NATIVE=$GGML_NATIVE \
 	-B "build_ggml" \
 	"ggml"
 
