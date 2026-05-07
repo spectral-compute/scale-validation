f483d699be608872728891b15facf4ed1c24f10a
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-05-07 16:49:49 +0100

    Remove failing microbenchmarks from building for the time being
    These include gels, prefetch, streamOrderedAllocation, and blas-fp8gemm. These fail for a variety of small reasons. Then there are several linking issues too. The linking issues seem to persist after filtering out these tests, so work continues.

--- HeCBench/00-clone.sh
+++ HeCBench/00-clone.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# Requires dvc
+# HeCBench requires `dvc` as part of its data management
+#
+# You can install this via `pipx`:
 #   sudo apt install -y pipx
 #   pipx ensurepath
 #   pipx install dvc
 #   pipx inject dvc dvc-s3
+#
+# or `uv`:
+#   curl -LsSf https://astral.sh/uv/install.sh | sh
+#   uv tool install dvc[s3]
+
+# TODO: What can we assume is installed on the running machine?
+# TODO: Uncomment if fine to install dependencies like this,
+#       else document.
+# if which uv &> /dev/null; then
+#     curl -LsSf https://astral.sh/uv/install.sh | sh
+#     uv tool install dvc[s3]
+# fi
 
 source "$(dirname "$0")"/../util/git.sh
 
@@ -12,7 +26,16 @@ do_clone_hash HeCBench https://github.com/ORNL/HeCBench.git "$(get_version HeCBe
 
 (
     cd HeCBench
+    
     dvc pull
+
     # The following does not compile even for cuda-nvidia
-    rm -rf dp4a-cuda
+    # TODO: Investigate if still failing
+    sed -i /dp4a/d src/CMakeLists.txt
+
+    # FIXME: These prevent the benchmark from compiling on gfx1201.
+    sed -i /gels/d src/CMakeLists.txt
+    sed -i /prefetch/d src/CMakeLists.txt
+    sed -i /streamOrderedAllocation/d src/CMakeLists.txt
+    sed -i /blas-fp8gemm/d src/CMakeLists.txt
 )
