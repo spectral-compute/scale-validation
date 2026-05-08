e496684362e2f10b87dadc6132bd6deb551ff188
Chris Kitching <chriskitching@linux.com>
2026-05-08 19:30:53 +0200

    Crazy hack to make CV-CUDA less broken util the compiler catches up

--- CV-CUDA/01-build.sh
+++ CV-CUDA/01-build.sh
@@ -6,7 +6,8 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="install" \
     -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
+    -DCMAKE_CXX_COMPILER="nvcc" \
+    -DCMAKE_CXX_FLAGS="-w -ferror-limit=0 -fcuda-nvcc-emulation -D__host__=\"__attribute__((host))\" -D__device__=\"__attribute__((device))\"" \
     -DBUILD_TESTS=ON \
     -B"build" \
     "CV-CUDA"
