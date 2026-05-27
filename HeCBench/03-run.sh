b249e21eb764d4656f2f49e3247fe28430ed47b4
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-05-27 09:15:04 +0100

    Output HeCBench results to CSV and to /tmp dir so CI can pick it up

--- HeCBench/03-run.sh
+++ HeCBench/03-run.sh
@@ -2,13 +2,16 @@
 set -u
 
 OUT_DIR="$(realpath .)/HeCBench"
+RESULTS_DIR="/tmp/ci-benchmarks/HeCBench"
 
 CUDA_ARCH_NUM="${CUDAARCHS#sm_}"
 
-DATA_FILE="$OUT_DIR/hecbench.cuda-sm$CUDA_ARCH_NUM.json"
+DATA_FILE="$OUT_DIR/hecbench.cuda-sm$CUDA_ARCH_NUM.csv"
+
+mkdir -p "$RESULTS_DIR"
 
 python3 $OUT_DIR/tools/hecbench run \
   --model cuda \
   --preset scale-cuda-sm$CUDA_ARCH_NUM \
   --store \
-  --output "$DATA_FILE"
+  --output "$RESULTS_DIR/$DATA_FILE"
