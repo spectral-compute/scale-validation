b834adcc38b91093aeafab0701078a3845091134
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-06-01 20:23:56 +0100

    HeCBench: Streamline clone docs, remove subshell use

--- HeCBench/00-clone.sh
+++ HeCBench/00-clone.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 # HeCBench requires `dvc` as part of its data management
-#
-# You can install this via `pipx`:
-#   sudo apt install -y pipx
-#   pipx ensurepath
-#   pipx install dvc
-#   pipx inject dvc dvc-s3
-#
-# or `uv`:
-#   curl -LsSf https://astral.sh/uv/install.sh | sh
-#   uv tool install dvc[s3]
-
-# TODO: What can we assume is installed on the running machine?
-# TODO: Uncomment if fine to install dependencies like this,
-#       else document.
-# if which uv &> /dev/null; then
-#     curl -LsSf https://astral.sh/uv/install.sh | sh
-#     uv tool install dvc[s3]
-# fi
+# We recommend installing via pipx or uv
+# See: https://doc.dvc.org/install/linux
 
 source "$(dirname "$0")"/../util/git.sh
 
 do_clone_hash HeCBench https://github.com/ORNL/HeCBench.git "$(get_version HeCBench)"
 
-(
-    cd HeCBench
-    
-    dvc pull
-)
+pushd HeCBench
+dvc pull
+popd
