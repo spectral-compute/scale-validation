983dc285a8a9b12ba5abc6f158082d6f648d11e3
SollyCB <solomon@spectralcompute.co.uk>
2026-06-02 14:29:21 +0000

    Specify c++17 name mangling
    Clang 18+ uses c++20 mangling, but pytorch expects c++17.

--- pytorch/03-build-pytorch.sh
+++ pytorch/03-build-pytorch.sh
@@ -72,6 +72,11 @@ export CMAKE_POLICY_VERSION_MINIMUM=3.5
 export FORCE_CUDA=1
 export MAX_JOBS=$(nproc)
 
+# clang >= 18 use c++20 mangling, but PyTorch expects c++17 mangling
+if [[ "$(nvcc --version)" == *" SCALE "* ]]; then
+  export CUDAFLAGS="-fclang-abi-compat=17 -Xcuda-ptxas -v"
+fi
+
 export CMAKE_ARGS="-DBUILD_BINARY=OFF -DBUILD_TEST=OFF"
 
 python setup.py build
