f414ac722828e6aeb44c88b0fe89451f76e066c5
SollyCB <solomon@spectralcompute.co.uk>
2026-06-02 16:39:24 +0000

    Move common stuff into one file
    Reduce noise inside of the build scripts, just gives one file to read
    that sets up the environment, and environment setup becomes one
    compartmentalised step for each build script.

--- pytorch/03-build-pytorch.sh
+++ pytorch/03-build-pytorch.sh
@@ -3,74 +3,8 @@ set -ETeuo pipefail
 shopt -s nullglob
 
 cd pytorch
+source $(dirname $0)/common.sh
 
-if [[ ! -d .venv ]]; then
-  python3 -m venv .venv
-fi
-
-source .venv/bin/activate
-python -m pip install --upgrade pip wheel
-python -m pip install "setuptools==81.0.0"
-python -m pip install pyyaml typing_extensions jinja2 numpy
-
-cd "build/pytorch"
-
-torch-arch() {
-  if ((${#CUDAARCHS} == 2)); then
-    echo "${CUDAARCHS:0:1}.${CUDAARCHS:1:1}"
-  else
-    echo "${CUDAARCHS:0:2}.${CUDAARCHS:2:1}"
-  fi
-}
-
-export CFLAGS="\
-    -march=native \
-    -mtune=native \
-    -Wno-inconsistent-missing-destructor-override \
-    -Wno-deprecated-copy-with-user-provided-dtor \
-    -Wno-dangling-reference \
-    -Wno-redundant-move
-"
-
-export CXXFLAGS="${CFLAGS}"
-export CUDNN_INCLUDE_DIR=/usr/include
-export CUDNN_LIB_DIR=/usr/lib
-export TORCH_CUDA_ARCH_LIST=$(torch-arch)
-export USE_CUDA=ON
-export USE_CUDNN=OFF
-export USE_CUFILE=OFF
-export CAFFE2_USE_CUDNN=OFF
-export USE_CUSPARSELT=OFF
-export USE_NCCL=OFF
-export USE_DISTRIBUTED=ON
-export USE_FAKELOWP=OFF
-export USE_FBGEMM=OFF
-export USE_FFMPEG=OFF
-export USE_GFLAGS=OFF
-export USE_GLOG=OFF
-export USE_ITT=OFF
-export USE_KINETO=OFF
-export USE_LEVELDB=OFF
-export USE_LMDB=OFF
-export USE_MKLDNN=OFF
-export USE_NUMPY=ON
-export USE_OPENCV=OFF
-export USE_ROCM=OFF
-export BUILD_CAFFE2=ON
-export BUILD_CAFFE2_OPS=ON
-export BUILD_BINARY=OFF
-export BUILD_TEST=OFF
-export CMAKE_POLICY_VERSION_MINIMUM=3.5
-export FORCE_CUDA=1
-export MAX_JOBS=$(nproc)
-export TORCH_USE_CUDA_DSA=OFF
-
-# clang >= 18 use c++20 mangling, but PyTorch expects c++17 mangling
-if [[ "$(nvcc --version)" == *" SCALE "* ]]; then
-  export CUDAFLAGS="-fclang-abi-compat=17 -Xcuda-ptxas -v"
-fi
-
-export CMAKE_ARGS="-DBUILD_BINARY=OFF -DBUILD_TEST=OFF"
-
+cd build/pytorch
 python setup.py build
 python setup.py install --root=$PWD/../../install --skip-build
--- pytorch/04-build-torchvision.sh
+++ pytorch/04-build-torchvision.sh
@@ -3,24 +3,12 @@
 set -ETeuo pipefail
 
 cd pytorch
-source .venv/bin/activate
-
-# Remove any previously installed incompatible torchvision
-python -m pip uninstall -y torchvision || true
-
-python -m pip install --upgrade numpy pillow ninja
-python -m pip install "setuptools==81.0.0"
+source $(dirname $0)/common.sh
 
 if [[ ! -d "vision" ]]; then
     source $(dirname $0)/../util/git.sh
     do_clone vision https://github.com/pytorch/vision.git v0.24.0
 fi
-
-# PyTorch has its own env var to set the CUDA compiler
-export PYTORCH_NVCC=${CUDACXX}
-
-# --generate-dependencies-with-compile is not a valid clang option
-export TORCH_EXTENSION_SKIP_NVCC_GEN_DEPENDENCIES=1
 
 cd vision
 python -m pip install -v --no-build-isolation --no-deps -e .
