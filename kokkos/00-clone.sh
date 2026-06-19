d3c5a71045bfcd40936697f671aa958e7bc90a86
Rei Stepanenko <rei@ftvkyo.me>
2026-06-19 14:20:01 +0100

    kokkos: use upstream, pin the version

--- kokkos/00-clone.sh
+++ kokkos/00-clone.sh
@@ -4,4 +4,4 @@ set -e
 
 source "$(dirname "$0")"/../util/git.sh
 
-git clone https://github.com/rbourgeois33/kokkos.git
+do_clone kokkos https://github.com/kokkos/kokkos.git "$(get_version kokkos)"
