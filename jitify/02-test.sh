126320a024361f1feabbd8fd18c4780a4b21a298
SollyCB <solomon@spectralcompute.co.uk>
2026-07-01 11:07:44 +0000

    Disable jitify test that fails for nv
    Need to check if it is scale specific.
    
    scale#1116

--- jitify/02-test.sh
+++ jitify/02-test.sh
@@ -3,5 +3,11 @@
 set -ETeuo pipefail
 
 cd jitify
+
+# TODO: https://code.spectralcompute.com/spectral-compute/scale/issues/1116
+if [[ ! $SCALE_ENV == gfx* ]]; then
+	FAILS_FOR_NV="JitifyTest.ClassKernelArg"
+fi
+
 # TODO: https://gitlab.com/spectral-ai/engineering/cuda/platform/redscale/-/issues/549
-./jitify_test --gtest_filter="-JitifyTest.ConstantMemory:JitifyTest.ConstantMemory_experimental:JitifyTest.RemoveUnusedGlobals:JitifyTest.LinkExternalFiles:JitifyTest.LinkCurrentExecutable"
+./jitify_test --gtest_filter="-JitifyTest.ConstantMemory:JitifyTest.ConstantMemory_experimental:JitifyTest.RemoveUnusedGlobals:JitifyTest.LinkExternalFiles:JitifyTest.LinkCurrentExecutable:${FAILS_FOR_NV:-}"
