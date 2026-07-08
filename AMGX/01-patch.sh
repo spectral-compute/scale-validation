02d7f6de46bcd5098c87c87af05e9f46253982b1
Chris Kitching <chriskitching@linux.com>
2026-07-08 17:25:25 +0200

    Remove EmptyKernel patches: compiler bug is fixed

--- AMGX/01-patch.sh
+++ AMGX/01-patch.sh
@@ -3,9 +3,6 @@
 set -e
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 
-# We need the patched version for now (empty kernel, #483;#506)
-patch -p0 -d "AMGX/thrust/dependencies/cub" < "${SCRIPT_DIR}/device_util.patch"
-
 # Patch an integer overflow in the Memory_Use_* tests
 patch -p0 -d "AMGX" < "${SCRIPT_DIR}/memory_use_int_overflow.patch"
 
