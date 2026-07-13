a45514f2a2e0e5a783c00dfcd0c4717ed724e85c
SollyCB <solomon@spectralcompute.co.uk>
2026-07-13 14:28:56 +0000

    Fix gromacs/03-benchMEM.sh
    Make the script actually exit with the error code of the test binary;
    don't output an effectively empty csv.

--- gromacs/03-benchMEM.sh
+++ gromacs/03-benchMEM.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# set -ETeuo pipefail
-# set -o xtrace
+set -eo pipefail
 
 # Download the benchmark data if it doesn't already exist.
 BENCH_MEM="data/MaxPlanckInstituteGromacsBenchmarks"
@@ -11,31 +10,7 @@ if [ ! -e "$BENCH_MEM/benchMEM.zip" ] ; then
     unzip "$BENCH_MEM/benchMEM.zip"
 fi
 
-# Create somewhere for results.
-RESULT_FILE="$(pwd)/$(basename -s .sh "$0").csv"
-rm -f "${RESULT_FILE}"
-
-RESULT_DIR="$(pwd)/benchmarks/MaxPlanckInstitute/benchMEM"
-mkdir -p "${RESULT_DIR}"
-
 source "install/bin/GMXRC"
 
-set +e
 # When comparing with hip we should use -pme cpu -bonded cpu -update cpu
-#gmx mdrun -s benchMEM.tpr -v -ntmpi 1 -pme cpu -bonded cpu -update cpu -nb gpu
-# When comparing with a more complete version
 gmx mdrun -s "benchMEM.tpr" -v -ntmpi 1 -nb gpu
-RESULT=$?
-set -e
-# Log to result CSV
-if [ "$RESULT" != 0 ]; then
-    echo "MPGbenchMEM,status,fail" >> "${RESULT_FILE}"
-else
-    echo "MPGbenchMEM,status,success" >> "${RESULT_FILE}"
-    echo "MPGbenchMEM,time,$(grep 'Time:' md.log | sed -E 's/ +/ /g' | cut -d ' ' -f 4)" >> "${RESULT_FILE}"
-fi
-
-# Pretty print result
-sed -E 's/(.*),status,(.*)/\1: \2/;s/(.*),time,(.*)/\1: \2 s/' "${RESULT_FILE}"
-
-echo "Done"
