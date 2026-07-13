6836929585029054c7a11d893ee5129975219745
SollyCB <solomon@spectralcompute.co.uk>
2026-07-13 13:45:26 +0000

    Return exit code of gromacs test script
    Returning the exit code of the `tee` command is not useful.

--- gromacs/02-gromacs-regression-test.sh
+++ gromacs/02-gromacs-regression-test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 source "install/bin/GMXRC"
 
 OUT="$(pwd)/regression-test.txt"
-
-set +e
 
 make -O -C build tests -j
 ctest --test-dir build 2>&1 | tee "${OUT}"
