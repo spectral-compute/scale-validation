3f56bab610e152ab4540e493d125af081c320d26
SollyCB <solomon@spectralcompute.co.uk>
2026-07-13 13:46:34 +0000

    Remove redundant lines in gromacs test script
    Inline a variable, remove gratuitous `pwd`.

--- gromacs/02-gromacs-regression-test.sh
+++ gromacs/02-gromacs-regression-test.sh
@@ -4,7 +4,5 @@ set -eo pipefail
 
 source "install/bin/GMXRC"
 
-OUT="$(pwd)/regression-test.txt"
-
 make -O -C build tests -j
-ctest --test-dir build 2>&1 | tee "${OUT}"
+ctest --test-dir build 2>&1 | tee "regression-test.txt"
