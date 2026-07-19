7794848775b84fdc0c8d415541a7236c7b924a94
SollyCB <solomon@spectralcompute.co.uk>
2026-07-19 13:46:16 +0000

    pytorch - Remove duplicated logic
    This is implemented in the `util/common.sh` file, and is unused in the
    build script.

--- pytorch/03-build-pytorch.sh
+++ pytorch/03-build-pytorch.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -ETeuo pipefail
-shopt -s nullglob
 
 CUDAARCHS="${CUDAARCHS:-89}"
-cudaarch_to_torch_arch() {
-    local arch="$1"
-    local major="${arch:0:${#arch}-1}"
-    local minor="${arch: -1}"
-    echo "${major}.${minor}"
-}
 
 cd pytorch
 source $(dirname $0)/util/common.sh
