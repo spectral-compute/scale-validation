1e3fa335379b5ec9627b80dcee3b7f5e64d498e8
SollyCB <solomon@spectralcompute.co.uk>
2026-07-22 18:59:57 +0000

    pytorch - Less fraught mnist requirements install
    Its `requirements.txt` only lists torch and vision, so it should be noop
    except for pulling in other unmet deps. But rather than risk it pulling
    in thirdparty builds of these things if our build or install of
    torch/vision ever breaks for some reason, and then us not noticing,
    let's just install what it wants without potentially shooting ourselves
    in the foot.

--- pytorch/06-run-example-mnist.sh
+++ pytorch/06-run-example-mnist.sh
@@ -8,8 +8,13 @@ source pytorch/.venv/bin/activate
 # Pytorch tries to use and other GPUs leading to errors.
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
 
+# mnist's requirements.txt only lists torch and torchvision. It should be a
+# noop to install deps via this file, except for pulling in deps, but rather
+# than risk having something break and CI mistakenly installing and running
+# the pip pytorch, let's just install pillow.
+python -m pip install pillow
+
 EPOCHS="${EPOCHS:-5}"
 
 cd examples/mnist
-python -m pip install -r requirements.txt
 python main.py --epochs "$EPOCHS"
