23f248ddd2c717f019b4cfbf9ae25dee0ee38532
SollyCB <solomon@spectralcompute.co.uk>
2026-07-22 18:58:55 +0000

    pytorch - Move stage specifics out of build-env.sh
    Issue #1156 is tracking the state of knowing what env vars are
    useful/required for building torch and vision; cannot unset any of these
    before knowing which are not redundant.
    
    But installing pip modules does not need to happen in build-env.sh;
    neither does creating the venv. This only needs to happen for building
    torch.

--- pytorch/02-build-pytorch.sh
+++ pytorch/02-build-pytorch.sh
@@ -5,5 +5,9 @@ SCRIPT_DIR="$(dirname "$(realpath $0)")"
 cd pytorch
 source "$SCRIPT_DIR/util/build-env.sh"
 
+python3 -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip -r requirements.txt
+
 python setup.py build
 python setup.py install --skip-build
--- pytorch/03-build-torchvision.sh
+++ pytorch/03-build-torchvision.sh
@@ -2,11 +2,13 @@
 set -ETeuo pipefail
 SCRIPT_DIR="$(dirname "$(realpath $0)")"
 
-cd pytorch
-source "$SCRIPT_DIR/util/build-env.sh"
-
 source "$SCRIPT_DIR/../util/git.sh"
+cd pytorch
 do_clone vision https://github.com/pytorch/vision.git v0.24.0
 
+# TODO(#1156): Are any of these env vars required/useful for torchvision?
+source "$SCRIPT_DIR/util/build-env.sh"
+
+source .venv/bin/activate
 cd vision
-python -m pip install -v --no-build-isolation --no-deps -e .
+python -m pip install -v --no-build-isolation -e .
