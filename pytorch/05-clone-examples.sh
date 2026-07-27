d0c1ce98e8e5fed772bf0b2f0d3329a64ace04d2
Matthew Ireland <matthew@ireland.cx>
2026-07-27 16:15:38 +0100

    pytorch: don't re-clone the examples repository if it already exists

--- pytorch/05-clone-examples.sh
+++ pytorch/05-clone-examples.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-git clone https://github.com/pytorch/examples.git examples
+
+if [ ! -d examples ]; then
+    git clone https://github.com/pytorch/examples.git examples
+else
+    (cd examples && git pull)
+fi
+
