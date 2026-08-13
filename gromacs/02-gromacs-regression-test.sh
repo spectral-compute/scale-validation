b81bb97a89f11506de41a8a94ac05d1568fc5da6
vgorl-spectral <vera@spectralcompute.co.uk>
2026-08-13 16:12:50 +0000

    Corrected naming scheme

--- gromacs/02-gromacs-regression-test.sh
+++ gromacs/02-gromacs-regression-test.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-source "install/bin/GMXRC"
+source "$BIN_INSTALL_PATH/bin/GMXRC"
 
 make -O -C build tests -j
 ctest --test-dir build --verbose 2>&1 | tee "regression-test.txt"
