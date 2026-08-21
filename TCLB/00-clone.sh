c33ab1902b3c5f03cb15ca46ab711070ef84df19
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:09:39 +0100

    tclb

--- TCLB/00-clone.sh
+++ TCLB/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone TCLB https://github.com/CFD-GO/TCLB.git "$(get_version TCLB)"
