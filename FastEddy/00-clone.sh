a03abc24f057189130bc6f17ba1e719d4a87f90a
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 11:33:30 +0100

    fasteddy

--- FastEddy/00-clone.sh
+++ FastEddy/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone FastEddy https://github.com/NCAR/FastEddy-model.git "$(get_version FastEddy)"
