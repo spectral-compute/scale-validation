321c09c480f8abfd641262d6ae9234fabc3590c9
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:05:48 +0100

    gomc

--- gomc/03-example.sh
+++ gomc/03-example.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
-set -ETeuo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 OUT_DIR=$(realpath .)
 cd "GOMC_Examples/NVT_GEMC/pure_fluid/octane_T_360_00_K"
 git clean -df .
 "${OUT_DIR}/build/GOMC_GPU_GEMC" in.conf 2>&1 | tee test.log
 
-if grep -F 'Warning: Updated energy differs from Recalculated Energy!' test.log ; then
-    echo "Error: Result is incorrect."
+if grep -F 'Warning: Updated energy differs from Recalculated Energy!' test.log; then
+    log "Error: Result is incorrect."
     exit 1
 fi
