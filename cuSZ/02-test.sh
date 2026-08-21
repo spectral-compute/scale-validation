862782bebb708897981567879f1f6fa54923e380
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:36:57 +0100

    cusz

--- cuSZ/02-test.sh
+++ cuSZ/02-test.sh
@@ -1,23 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
+# shellcheck disable=SC2164
 cd "./build/test"
 
 export LD_LIBRARY_PATH="${CUDA_PATH}/lib"
-for F in $(find . -maxdepth 1 -type f -perm /u+x | grep -vE '\.so$' | sort) ; do
-    echo -e "\x1b[1m${F}\x1b[m"
+for F in $(find . -maxdepth 1 -type f -perm /u+x | grep -vE '\.so$' | sort); do
+    echo -e "${F}"
     case "${F}" in
-        ./l1_compact)
-            echo -e '\x1b[31;1mCrashes on Nvidia\x1b[m'
+    ./l1_compact)
+        log 'Crashes on Nvidia'
         ;;
-        ./spv_cu)
-            "${F}" || true
-            echo -e '\x1b[33;1mBroken on Nvidia\x1b[m'
+    ./spv_cu)
+        "${F}" || true
+        log 'Broken on Nvidia'
         ;;
-        *)
-            "${F}"
+    *)
+        "${F}"
         ;;
     esac
 done
 
+# shellcheck disable=SC2103,SC2164
 cd -
