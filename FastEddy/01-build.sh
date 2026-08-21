d9332edda2017aa1278e44b32bfbc3e0549619e6
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 11:33:30 +0100

    fasteddy

--- FastEddy/01-build.sh
+++ FastEddy/01-build.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 OUT_DIR=$(realpath ../)
-if [ ! -e "${OUT_DIR}/openmpi/install" ] ; then
+if [ ! -e "${OUT_DIR}/openmpi/install" ]; then
     echo "Please build the OpenMPI third party project first. Use the same working directory." 1>&2
     exit 1
 fi
@@ -14,8 +13,9 @@ export LD_LIBRARY_PATH="${OUT_DIR}/openmpi/install/lib:${LD_LIBRARY_PATH}"
 # Amusing hack to make it use the SCALE MPI.
 export NCAR_ROOT_MPI=${OUT_DIR}/openmpi/install
 
-if [ -z $NCAR_ROOT_MPI ]; then
-  echo "Run the openmpi script first"
+if [ -z "$NCAR_ROOT_MPI" ]; then
+    log "Run the openmpi script first"
+    exit 1
 fi
 
 make -O -C FastEddy/SRC/FEMAIN
