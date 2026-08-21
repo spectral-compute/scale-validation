5ef96885f3216fefec51abd02eeafd54bec8b7a0
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:09:53 +0100

    GPUJPEG

--- GPUJPEG/01-patch.sh
+++ GPUJPEG/01-patch.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-
-patch -p0 -d "GPUJPEG" < "${SCRIPT_DIR}/ld.patch"
+patch -p0 -d "GPUJPEG" <"${SCRIPT_DIR}/ld.patch"
