799040bc5defd884bd32a317347ad35b14419111
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:35:06 +0100

    cugraph

--- cugraph/00-clone.sh
+++ cugraph/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash cugraph https://github.com/rapidsai/cugraph "$(get_version cugraph)"
+do_clone cugraph https://github.com/rapidsai/cugraph "$(get_version cugraph)"
