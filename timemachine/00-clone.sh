3d6a3cd8f3228f1124b98ea2c19abe9a62950d23
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:18:59 +0100

    timemachine

--- timemachine/00-clone.sh
+++ timemachine/00-clone.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash timemachine https://github.com/proteneer/timemachine.git "$(get_version timemachine)"
-
-# Upstream bug means `CMAKE_CXX_FLAGS` fails with this project,
-# so disable Werror a weirder way:
-sed -Ee 's/-Werror ?(all-warnings ?)?//g' -i"" timemachine/cpp/CMakeLists.txt
+do_clone timemachine https://github.com/proteneer/timemachine.git "$(get_version timemachine)"
