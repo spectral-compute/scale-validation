1f49bd84efdaad551857213f54d6b0fd5ed4125b
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 11:44:40 +0100

    flashinfer

--- flashinfer/01-build.sh
+++ flashinfer/01-build.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
-
-set -ETeuo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 export SCALE_CUDA_VERSION="12.1"
 
-# Configure.
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_C_COMPILER="clang"
+    -DCMAKE_CXX_COMPILER="clang++"
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -Dflashinfer_NVCC_ARCHS="${CUDAARCHS}"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCMAKE_CUDA_FLAGS="-Wno-unused-result -Wno-deprecated-declarations"
+
+    -DFLASHINFER_ENABLE_FP8=OFF
+    -DFLASHINFER_ENABLE_FP8_E4M3=OFF
+    -DFLASHINFER_ENABLE_FP8_E5M2=OFF
+    -DFLASHINFER_ENABLE_F16=ON
+    -DFLASHINFER_ENABLE_BF16=ON
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -Dflashinfer_NVCC_ARCHS="${CUDAARCHS}" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCMAKE_CUDA_FLAGS="-Wno-unused-result -Wno-deprecated-declarations" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DCMAKE_C_COMPILER="clang" \
-    -DFLASHINFER_ENABLE_FP8=OFF \
-    -DFLASHINFER_ENABLE_FP8_E4M3=OFF \
-    -DFLASHINFER_ENABLE_FP8_E5M2=OFF \
-    -DFLASHINFER_ENABLE_F16=ON \
-    -DFLASHINFER_ENABLE_BF16=ON \
+    "${args[@]}" \
     -B"build" \
     "flashinfer"
 
-# Build.
 cmake --build "build" -j"$(nproc)"
