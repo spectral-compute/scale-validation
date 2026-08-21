b311bae527728e5d8114f3e21e5660110f309ca1
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:41:02 +0100

    opencv

--- opencv/00-clone-opencv-and-test-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash opencv https://github.com/opencv/opencv.git "$(get_version opencv)"
-do_clone_hash opencv_contrib https://github.com/opencv/opencv_contrib.git "$(get_version opencv_contrib)"
-do_clone_hash opencv_extra https://github.com/opencv/opencv_extra.git "$(get_version opencv_extra)"
