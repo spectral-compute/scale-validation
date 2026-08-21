5d070a587bd0594e4593c54cf58fbf93d442b2f0
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:33:04 +0100

    MAGMA

--- MAGMA/00-clone.sh
+++ MAGMA/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone MAGMA https://github.com/icl-utk-edu/magma/ "$(get_version MAGMA)"
