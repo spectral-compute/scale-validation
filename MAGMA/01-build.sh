cc3cf705e51015a5cce2f3730a5d12adeef91249
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:33:04 +0100

    MAGMA

--- MAGMA/01-build.sh
+++ MAGMA/01-build.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # This is incredibly cursed, but it's the official instructions!
 # This generates part of the cmake build system using make.
-echo -e "BACKEND = cuda\nFORT = true\nGPU_TARGET=sm_${CUDAARCHS}" > MAGMA/make.inc
+echo -e "BACKEND = cuda\nFORT = true\nGPU_TARGET=sm_${CUDAARCHS}" >MAGMA/make.inc
 make -O -C MAGMA -j"$(nproc)" generate
 
 sed -i"" -Ee 's|find_package\( *OpenMP *\)||g' "MAGMA/CMakeLists.txt"
 
-# Configure.
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -DCMAKE_C_COMPILER="clang"
+    -DCMAKE_CXX_COMPILER="clang++"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCMAKE_INSTALL_PREFIX="${OUT_DIR}/MAGMA/MAGMA/inst"
+
+    -DMAGMA_ENABLE_CUDA=ON
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${OUT_DIR}/MAGMA/MAGMA/inst" \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DMAGMA_ENABLE_CUDA=ON \
+    "${args[@]}" \
     -B"build" \
     "MAGMA"
 
