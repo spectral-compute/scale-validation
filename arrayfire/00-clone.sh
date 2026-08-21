453f5c3f2e79133a6814ec07b6f39a0d38c1f54d
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:42:36 +0100

    arrayfire

--- arrayfire/00-clone.sh
+++ arrayfire/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone arrayfire https://github.com/arrayfire/arrayfire.git "$(get_version arrayfire)"
