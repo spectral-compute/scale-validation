3d44e9bdf7696ba271952fdce70d288860c85807
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:24:33 +0100

    AMGX

--- AMGX/01-patch.sh
+++ AMGX/01-patch.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-set -e
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # Patch an integer overflow in the Memory_Use_* tests
-patch -p0 -d "AMGX" < "${SCRIPT_DIR}/memory_use_int_overflow.patch"
+patch -p0 -d "AMGX" <"${SCRIPT_DIR}/memory_use_int_overflow.patch"
 
 # Patch a legitimate bug in AMGX that we warn about.
 sed -E \
-  's/description\(p.description\), default_value\(default_value\)/description(p.description), default_value(p.default_value)/' \
-  -i "AMGX/include/amg_config.h"
+    's/description\(p.description\), default_value\(default_value\)/description(p.description), default_value(p.default_value)/' \
+    -i "AMGX/include/amg_config.h"
--- AMGX/04-example.sh
+++ AMGX/04-example.sh
@@ -1,29 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
-
-MPI_PATH=$(realpath ../openmpi/install)
-SRC_DIR=$(realpath ./AMGX)
+MPI_PATH="$(realpath ../openmpi/install)"
 
 export PATH="${MPI_PATH}/bin:${CUDA_DIR}/bin:${PATH}"
 export LD_LIBRARY_PATH="${MPI_PATH}/lib:${LD_LIBRARY_PATH}"
 export OMPI_MCA_accelerator=cuda
 
-for MODE in dFFI dDDI ; do
+for MODE in dFFI dDDI; do
     echo -e "\x1b[34;1mVariant\x1b[m: \x1b[1m${MODE}\x1b[m"
     build/examples/amgx_capi \
-        -m ${SRC_DIR}/examples/matrix.mtx \
-        -c ${SRC_DIR}/src/configs/FGMRES_AGGREGATION.json \
-        -mode ${MODE}
+        -m "${SRC_DIR}/examples/matrix.mtx" \
+        -c "${SRC_DIR}/src/configs/FGMRES_AGGREGATION.json" \
+        -mode "${MODE}"
 
     echo -e "\x1b[34;1mVariant\x1b[m: \x1b[1m${MODE} with MPI\x1b[m"
-    if [ ! -z "$(which scalediag)" ] && ! scalediag full-driver p2p ; then
+    if [ ! -z "$(which scalediag)" ] && ! scalediag full-driver p2p; then
         echo "Skipping due to lack of support"
         continue
     fi
     mpirun -np 2 \
         build/examples/amgx_mpi_capi \
-        -m ${SRC_DIR}/examples/matrix.mtx \
-        -c ${SRC_DIR}/src/configs/FGMRES_AGGREGATION.json \
-        -mode ${MODE}
+        -m "${SRC_DIR}/examples/matrix.mtx" \
+        -c "${SRC_DIR}/src/configs/FGMRES_AGGREGATION.json" \
+        -mode "${MODE}"
 done
