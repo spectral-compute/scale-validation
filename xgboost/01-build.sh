7569cdb09fd71cf84f77317ff1eb7643e42c6ee8
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:25:31 +0100

    xgboost

--- xgboost/01-build.sh
+++ xgboost/01-build.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # Configure.
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX="install"
+    -DCMAKE_C_COMPILER="clang"
+    -DCMAKE_CXX_COMPILER="clang++"
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+
+    -DUSE_CUDA=ON
+
+    -DKEEP_BUILD_ARTIFACTS_IN_BINARY_DIR=ON
+    -DGOOGLE_TEST=ON
+)
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="install" \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DUSE_CUDA=ON \
-    -DKEEP_BUILD_ARTIFACTS_IN_BINARY_DIR=ON \
-    -DGOOGLE_TEST=ON \
+    "${args[@]}" \
     -B"build" \
     "xgboost"
 
@@ -28,7 +32,7 @@ python3 -m installer "xgboost/python-package/dist"/*.whl \
 # For some reason, Arch and Ubuntu disagree on whether this directory gets created.
 
 PY_VER_PATH=$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2) # Like "3.12"
-if [ -e "install/lib/python${PY_VER_PATH}" ] ; then
+if [ -e "install/lib/python${PY_VER_PATH}" ]; then
     rm "install/lib/python${PY_VER_PATH}/site-packages/xgboost/lib/libxgboost.so"
     ln -s ../../../../libxgboost.so "install/lib/python${PY_VER_PATH}/site-packages/xgboost/lib/libxgboost.so"
 fi
