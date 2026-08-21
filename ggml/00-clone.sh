fa37cf1f1b7d73b1bd7b1d58fce40700d5c20876
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 11:47:00 +0100

    ggml

--- ggml/00-clone.sh
+++ ggml/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash ggml https://github.com/ggml-org/ggml "$(get_version ggml)"
+do_clone ggml https://github.com/ggml-org/ggml "$(get_version ggml)"
