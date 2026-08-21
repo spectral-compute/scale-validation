0b35296e3b6a630b4bacbfa2d6856ba90e4f03f6
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:19:47 +0100

    tiny-cuda-nn

--- tiny-cuda-nn/00-clone.sh
+++ tiny-cuda-nn/00-clone.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone tiny-cuda-nn https://github.com/NVlabs/tiny-cuda-nn "$(get_version tiny-cuda-nn)"
-
-# Patch that disables jit fusion to alow the use of cutlass.
-# Also adds CutlassMLP in nn configuration file and reduces
-# the number of iterations to avoid huge executions.
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-git -C tiny-cuda-nn apply "${SCRIPT_DIR}/enable_cutlass.patch"
--- tiny-cuda-nn/02-examples.sh
+++ tiny-cuda-nn/02-examples.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
-set -euo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 ./build/mlp_learning_an_image tiny-cuda-nn/data/images/albert.jpg 10000
