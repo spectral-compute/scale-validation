8305ec2f930f6ebab5a63bb2d5f8fef151bc0b4a
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:43:43 +0100

    bitnet

--- bitnet/00-clone.sh
+++ bitnet/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash bitnet https://github.com/microsoft/BitNet.git "$(get_version bitnet)"
+do_clone bitnet https://github.com/microsoft/BitNet.git "$(get_version bitnet)"
