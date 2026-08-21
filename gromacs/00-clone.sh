75840df1466685e8dd872eac0fa5628093f35b55
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:14:13 +0100

    gromacs

--- gromacs/00-clone.sh
+++ gromacs/00-clone.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
-
-GROMACS_VER="$(get_version gromacs)"
-
-do_clone gromacs https://github.com/gromacs/gromacs.git ${GROMACS_VER}
-
-# Fix cudaDeviceReset being called too early / in a race, which is undefined behaviour.
-git -C gromacs apply "$(dirname "$0")/gromacs_patches.diff"
+do_clone gromacs https://github.com/gromacs/gromacs.git "$(get_version gromacs)"
--- gromacs/02-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-source "install/bin/GMXRC"
-
-make -O -C build tests -j
-ctest --test-dir build --verbose 2>&1 | tee "regression-test.txt"
