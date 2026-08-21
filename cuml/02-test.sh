b05c896c63d0683cb1bf6381bba80ec47250e54c
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:36:05 +0100

    cuml

--- cuml/02-test.sh
+++ cuml/02-test.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-./cuml/cpp/build/test/ml # Single GPU algorithm tests
+./cuml/cpp/build/test/ml    # Single GPU algorithm tests
 ./cuml/cpp/build/test/ml_mg # Multi GPU algorithm tests, if --singlegpu was not used
 ./cuml/cpp/build/test/prims # ML Primitive function tests
