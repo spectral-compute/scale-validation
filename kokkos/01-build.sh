c1f21aa8896b31c19c16f8112ca8559bc03195f3
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:25:46 +0100

    kokkos

--- kokkos/01-build.sh
+++ kokkos/01-build.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 #for nvcc:
 #    -DCMAKE_CXX_COMPILER=$PWD/kokkos/bin/nvcc_wrapper \
 
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DKokkos_ENABLE_CUDA=ON
+    -DKokkos_ARCH_AMPERE86=ON
+    -DCMAKE_CXX_COMPILER=clang++
+    -DKokkos_ENABLE_TESTS=ON
+    -DCMAKE_CUDA_FLAGS="-DSCALE_WARP_SIZE=32"
+    -DCMAKE_CXX_FLAGS="-DSCALE_WARP_SIZE=32"
+    -DCMAKE_C_FLAGS="-DSCALE_WARP_SIZE=32"
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DKokkos_ENABLE_CUDA=ON \
-    -DKokkos_ARCH_AMPERE86=ON \
-    -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_CUDA_FLAGS="-DSCALE_WARP_SIZE=32" \
-    -DCMAKE_CXX_FLAGS="-DSCALE_WARP_SIZE=32" \
-    -DCMAKE_C_FLAGS="-DSCALE_WARP_SIZE=32" \
-    -DKokkos_ENABLE_TESTS=ON \
+    "${args[@]}" \
     -B"build" \
     "kokkos"
 
