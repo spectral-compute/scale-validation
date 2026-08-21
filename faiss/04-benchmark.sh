dc6897dd1349ffd4af00f4b87203664479a8636e
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 11:28:55 +0100

    faiss

--- faiss/04-benchmark.sh
+++ faiss/04-benchmark.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # Look for the Python package. These end up in different places on Arch and Ubuntu.
-
 PY_VER_PATH=$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2) # Like "3.12"
 for D in "install/lib/python${PY_VER_PATH}/site-packages" \
-         "install/local/lib/python${PY_VER_PATH}/dist-packages" ; do
-    if [ -e "${D}" ] ; then
+    "install/local/lib/python${PY_VER_PATH}/dist-packages"; do
+    if [ -e "${D}" ]; then
         export PYTHONPATH="${D}"
         break
     fi
 done
-if [ -z "${PYTHONPATH:-}" ] ; then
+if [ -z "${PYTHONPATH:-}" ]; then
     echo "Could not find built faiss Python package." 1>&2
     exit 1
 fi
