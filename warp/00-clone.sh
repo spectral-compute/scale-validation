42ecc80c9674bca19c9a6ec63f6e4e40d67f366c
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:21:58 +0100

    warp

--- warp/00-clone.sh
+++ warp/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash warp  https://github.com/NVIDIA/warp.git "$(get_version warp)"
+do_clone warp https://github.com/NVIDIA/warp.git "$(get_version warp)"
--- warp/01-build.sh
+++ warp/01-build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 cd warp
 python3 build_lib.py --use-dynamic-cuda
