dd1c2ce3e093d8b44bcb2bd210e18906d8e5dcdc
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:29:22 +0100

    llama.cpp

--- llama.cpp/llama_cpp_python/03-test.sh
+++ llama.cpp/llama_cpp_python/03-test.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../../util/prelude.sh
 
-set -ETeuo pipefail
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
-source "${SCRIPT_DIR}"/../../util/args.sh "$@"
-source "${SCALE_DIR}/bin/scaleenv" gfx1100
+source "${OUT_DIR}/llama-cpp-python/llama_cpp_python_scale_venv/bin/activate"
 
-source ${OUT_DIR}/llama-cpp-python/llama_cpp_python_scale_venv/bin/activate
-
-cd ${OUT_DIR}/llama-cpp-python/llama-cpp-python
+cd "${OUT_DIR}/llama-cpp-python/llama-cpp-python"
 
 pip install pytest
 pip install scipy
