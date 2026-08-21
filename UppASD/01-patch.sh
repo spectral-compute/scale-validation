5e50123fbe15ad703d1cc727062d560dc3d3cab0
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:20:35 +0100

    uppasd

--- UppASD/01-patch.sh
+++ UppASD/01-patch.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
-set -e
-SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # If building on a different machine than running on, you can set NO_TUNE_NATIVE=1 to
 # avoid SIGILL errors.
-patch -p0 -d "UppASD" < "${SCRIPT_DIR}/honor_no_tune_native.patch"
+patch -p0 -d "UppASD" <"${SCRIPT_DIR}/honor_no_tune_native.patch"
