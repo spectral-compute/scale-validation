06ed7d5cb976405ed931314d71296100c6dfad0c
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:21:10 +0100

    hecbench

--- HeCBench/00-clone.sh
+++ HeCBench/00-clone.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-set -ETeuo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # HeCBench requires `dvc` as part of its data management
 # We recommend installing via pipx or uv
 # See: https://doc.dvc.org/install/linux
 
-source "$(dirname "$0")"/../util/git.sh
+do_clone HeCBench https://github.com/ORNL/HeCBench.git "$(get_version HeCBench)"
 
-do_clone_hash HeCBench https://github.com/ORNL/HeCBench.git "$(get_version HeCBench)"
-
-pushd HeCBench
-dvc pull
-popd
+(cd HeCBench && dvc pull)
