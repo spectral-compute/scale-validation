2f032bc77271ceb1426e98914eb00e933381e777
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:45:02 +0100

    Bonsai

--- Bonsai/02-build.sh
+++ Bonsai/02-build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -euo pipefail
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+
+    -DCMAKE_C_COMPILER="clang"
+    -DCMAKE_CXX_COMPILER="clang++"
+    -DCUDA_NVCC_FLAGS="-gencode;arch=compute_${CUDAARCHS},code=sm_${CUDAARCHS}"
+
+    -DUSE_MPI=OFF
+    -DUSE_MPIMT=OFF
+    -DUSE_CUB=OFF
+    -DUSE_DUST=OFF
+    -DUSE_OPENGL=OFF
+)
 
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DCUDA_NVCC_FLAGS="-gencode;arch=compute_${CUDAARCHS},code=sm_${CUDAARCHS}" \
-    -DUSE_MPI=OFF \
-    -DUSE_MPIMT=OFF \
-    -DUSE_CUB=OFF \
-    -DUSE_DUST=OFF \
-    -DUSE_OPENGL=OFF \
+    "${args[@]}" \
     -B"build" \
     "Bonsai/runtime"
 
