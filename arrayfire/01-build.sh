1c428624acf3c69f98924e69e4484b36a7ee97f0
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:42:36 +0100

    arrayfire

--- arrayfire/01-build.sh
+++ arrayfire/01-build.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -DCUDA_TOOLKIT_ROOT_DIR="${CUDA_PATH}"
 
-# Configure.
+    -DAF_BUILD_OPENCL=OFF
+    -DAF_BUILD_CUDA=ON
+    -DAF_BUILD_DOCS=OFF
+    -DAF_BUILD_ONEAPI=OFF
+    -DAF_WITH_CUDNN=OFF
+    -DAF_WITH_NONFREE=ON
+    -DAF_WITH_FMT_HEADER_ONLY=ON
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCUDA_TOOLKIT_ROOT_DIR="${CUDA_PATH}" \
-    -DAF_BUILD_OPENCL=OFF \
-    -DAF_BUILD_CUDA=ON \
-    -DAF_BUILD_DOCS=OFF \
-    -DAF_BUILD_ONEAPI=OFF \
-    -DAF_WITH_CUDNN=OFF \
-    -DAF_WITH_NONFREE=ON \
-    -DAF_WITH_FMT_HEADER_ONLY=ON \
+    "${args[@]}" \
     -B"build" \
     "arrayfire"
 
