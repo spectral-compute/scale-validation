70701477d02f12138995c2118270006815d94514
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:07:24 +0100

    gpu_jpeg2k

--- gpu_jpeg2k/00-clone.sh
+++ gpu_jpeg2k/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash gpu_jpeg2k https://github.com/ePirat/gpu_jpeg2k "$(get_version gpu_jpeg2k)"
+do_clone gpu_jpeg2k https://github.com/ePirat/gpu_jpeg2k "$(get_version gpu_jpeg2k)"
--- gpu_jpeg2k/01-build.sh
+++ gpu_jpeg2k/01-build.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-SM_NUM=${CUDAARCHS}
+SM_NUM="${CUDAARCHS}"
 
 # This project is pretty ancient and has its own rather loopy way to get flags into the
 # nvcc call. CMake list syntax is required in CUDA_NVCC_FLAGS_RELEASE.
+args=(
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_CUDA_ARCHITECTURES="${SM_NUM}"
+    -DCMAKE_C_FLAGS="-Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-deprecated-declarations"
+    -DCMAKE_CXX_FLAGS="-Wno-unused-result -Wno-stringop-overread -Wno-switch -Wno-deprecated-declaration"
+    -DCUDA_NVCC_FLAGS_RELEASE="-gencode;arch=compute_${SM_NUM},code=sm_${SM_NUM};-Wno-unused-result;-Wno-switch;-Wno-error=implicit-const-int-float-conversion;-Wno-deprecated-declarations"
+)
+
 cmake \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CUDA_ARCHITECTURES="${SM_NUM}" \
-    -DCMAKE_C_FLAGS="-Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-deprecated-declarations" \
-    -DCMAKE_CXX_FLAGS="-Wno-unused-result -Wno-stringop-overread -Wno-switch -Wno-deprecated-declaration" \
-    -DCUDA_NVCC_FLAGS_RELEASE="-gencode;arch=compute_${SM_NUM},code=sm_${SM_NUM};-Wno-unused-result;-Wno-switch;-Wno-error=implicit-const-int-float-conversion;-Wno-deprecated-declarations" \
+    "${args[@]}" \
     -B"build" \
     "gpu_jpeg2k"
 
-# Build.
 cmake --build "build" -j"$(nproc)"
