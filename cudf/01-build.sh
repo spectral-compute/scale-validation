6c00acbd086bea75e65946136694c88620c93720
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:34:31 +0100

    cudf

--- cudf/01-build.sh
+++ cudf/01-build.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
-cd cudf
-./build.sh --pydevelop libcudf libcudf_kafka cudf dask_cudf cudf_kafka custreamz
-cd -
+(cd cudf && ./build.sh --pydevelop libcudf libcudf_kafka cudf dask_cudf cudf_kafka custreamz)
--- cudf/02-test.sh
+++ cudf/02-test.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
-
+# shellcheck disable=SC2164
 cd cudf/cudf
-ctest --test-dir ${CUDF_HOME}/cpp/build  # libcudf
-ctest --test-dir ${CUDF_HOME}/cpp/libcudf_kafka/build  # libcudf_kafka
+ctest --test-dir "${CUDF_HOME}/cpp/build"               # libcudf
+ctest --test-dir "${CUDF_HOME}/cpp/libcudf_kafka/build" # libcudf_kafka
 pytest -v python/cudf/cudf/tests
 pytest -v python/dask_cudf/dask_cudf/ # There are tests in both tests/ and io/tests/
 pytest -v python/custreamz/custreamz/tests
