f7a240299bc0d84ce58713af0dd83c1ceabd0a59
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:02:43 +0100

    cudahandbook

--- cudahandbook/00-clone.sh
+++ cudahandbook/00-clone.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-set -ETeuo pipefail
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # cudahandbook doesn't do tags, releases, or release branches, it seems.
-do_clone_hash cudahandbook https://github.com/ArchaeaSoftware/cudahandbook.git "$(get_version cudahandbook)"
+do_clone cudahandbook https://github.com/ArchaeaSoftware/cudahandbook.git "$(get_version cudahandbook)"
