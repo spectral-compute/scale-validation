133c813b1d87427e9329a0c812219574df60a782
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:25:31 +0100

    xgboost

--- xgboost/03-examples.sh
+++ xgboost/03-examples.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
-
-set -ETeuo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 # Look for the Python package. These end up in different places on Arch and Ubuntu.
 
 PY_VER_PATH=$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2) # Like "3.12"
 for D in "install/lib/python${PY_VER_PATH}/site-packages" \
-         "install/local/lib/python${PY_VER_PATH}/dist-packages" ; do
-    if [ -e "${D}" ] ; then
+    "install/local/lib/python${PY_VER_PATH}/dist-packages"; do
+    if [ -e "${D}" ]; then
         export PYTHONPATH="${D}"
         break
     fi
 done
-if [ -z "${PYTHONPATH:-}" ] ; then
+if [ -z "${PYTHONPATH:-}" ]; then
     echo "Could not find built xgboost Python package." 1>&2
     exit 1
 fi
 
-THIS_PATH="$(realpath $(dirname "$0"))"
-
-for booster in gbtree dart ; do
-    for tree_method in hist approx ; do
-        echo -e "\x1b[32;1mHouse prices ${booster} ${tree_method} example\x1b[m"
+for booster in gbtree dart; do
+    for tree_method in hist approx; do
+        echo -e "House prices ${booster} ${tree_method} example"
         cd "House-Prices-Advanced-Regression"
-        python3 "${THIS_PATH}/example-house-prices.py" ${booster} ${tree_method}
+        python3 "${SCRIPT_DIR}/example-house-prices.py" ${booster} ${tree_method}
 
-        echo -e "\x1b[32;1mWeather ${booster} ${tree_method} example\x1b[m"
+        echo -e "Weather ${booster} ${tree_method} example"
         cd "datasets"
-        python3 "${THIS_PATH}/example-weather.py" ${booster} ${tree_method}
+        python3 "${SCRIPT_DIR}/example-weather.py" ${booster} ${tree_method}
     done
 done
