5a8ffe4688b2de7842e95526b15e57e906ca12d5
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:24:33 +0100

    AMGX

--- AMGX/02-build.sh
+++ AMGX/02-build.sh
@@ -1,26 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
-
-OUT_DIR=$(realpath ../)
-if [ ! -e "${OUT_DIR}/openmpi/install" ] ; then
-    echo "Please build the OpenMPI third party project first. Use the same working directory." 1>&2
+OUT_DIR="$(realpath ../)"
+if [ ! -e "${OUT_DIR}/openmpi/install" ]; then
+    log "Please build the OpenMPI third party project first. Use the same working directory."
     exit 1
 fi
 
 # Configure.
-if [ -z "$(which scalediag)" ] || scalediag full-driver p2p ; then
+if [ -z "$(which scalediag)" ] || scalediag full-driver p2p; then
+    log "scalediag did not detect p2p, not using MPI"
     CMAKE_NO_MPI=Off
 else
+    log "scalediag did detect p2p, using MPI"
     CMAKE_NO_MPI=On
 fi
 
+args=(
+    -DCMAKE_NO_MPI="${CMAKE_NO_MPI}"
+
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCUDA_ARCH="${CUDAARCHS}"
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCUDA_ARCH="${CUDAARCHS}" \
-    -DCMAKE_NO_MPI=${CMAKE_NO_MPI} \
+    "${args[@]}" \
     -B"build" \
     "AMGX"
 
