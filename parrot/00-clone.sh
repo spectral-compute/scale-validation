9079649987fb09a936d0c62dd03f702aa6f2aed0
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:44:54 +0100

    parrot

--- parrot/00-clone.sh
+++ parrot/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash parrot https://github.com/NVlabs/parrot.git c88d995
+do_clone parrot https://github.com/NVlabs/parrot.git "$(get_version parrot)"
