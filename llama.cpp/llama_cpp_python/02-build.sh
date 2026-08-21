6938bcaa9b747b0c9df61cec4ccc14f213c32cbf
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:41:30 +0100

    remove unttested llama.cpp stuff

--- llama.cpp/llama_cpp_python/02-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-. "$(dirname "$0")"/../../util/prelude.sh
-
-python3 -m venv "${OUT_DIR}/llama-cpp-python/llama_cpp_python_scale_venv"
-source "${OUT_DIR}/llama-cpp-python/llama_cpp_python_scale_venv/bin/activate"
-
-CMAKE_ARGS="-DGGML_CUDA=on" pip install "${OUT_DIR}/llama-cpp-python/llama-cpp-python" --no-cache-dir --force-reinstall
