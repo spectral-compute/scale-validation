8323742949a89af79136503d4d5232b24ad72f1c
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:15:54 +0100

    hashcat

--- hashcat/02-test-short.sh
+++ hashcat/02-test-short.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
-set -e
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 mkdir -p out
 
 HASH="$(echo -n test | sha256sum | cut -d ' ' -f 1)"
 ./build/hashcat --backend-ignore-hip --backend-ignore-opencl --potfile-disable -O -o out/test.txt -m 1400 -a 3 "${HASH}"
 
-if [ "$(cat out/test.txt)" == "${HASH}:test" ] ; then
+if [ "$(cat out/test.txt)" == "${HASH}:test" ]; then
     exit 0
 fi
 
