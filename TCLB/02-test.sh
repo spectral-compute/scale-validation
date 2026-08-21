6e6b0cd8745b6db634c862ae11c30538276e18a3
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:09:39 +0100

    tclb

--- TCLB/02-test.sh
+++ TCLB/02-test.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
 export OMPI_MCA_accelerator=cuda
 
-cd "./TCLB"
-./CLB/d2q9/main ./example/flow/2d/karman.xml
+cd "./TCLB" && ./CLB/d2q9/main ./example/flow/2d/karman.xml
