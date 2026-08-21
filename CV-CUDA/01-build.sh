96e6a8dd6f355b647d7880c31491bf1b2d053980
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 10:53:13 +0100

    CV-CUDA

--- CV-CUDA/01-build.sh
+++ CV-CUDA/01-build.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX="install"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCMAKE_CXX_COMPILER="nvcc"
+
+    -DCMAKE_CXX_FLAGS="-w -ferror-limit=0 -fcuda-nvcc-emulation -D__host__=\"__attribute__((host))\" -D__device__=\"__attribute__((device))\""
+
+    -DBUILD_TESTS=ON
+)
 
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="install" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCMAKE_CXX_COMPILER="nvcc" \
-    -DCMAKE_CXX_FLAGS="-w -ferror-limit=0 -fcuda-nvcc-emulation -D__host__=\"__attribute__((host))\" -D__device__=\"__attribute__((device))\"" \
-    -DBUILD_TESTS=ON \
+    "${args[@]}" \
     -B"build" \
     "CV-CUDA"
 
