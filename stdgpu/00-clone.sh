c6d3a0a036a93e5f0b752bca8ce79ea600d62a6a
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:07:45 +0100

    stdgpu

--- stdgpu/00-clone.sh
+++ stdgpu/00-clone.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -e
-
-source "$(dirname "$0")"/../util/git.sh
-
-do_clone_hash stdgpu https://github.com/stotko/stdgpu.git "$(get_version stdgpu)"
+do_clone stdgpu https://github.com/stotko/stdgpu.git "$(get_version stdgpu)"
