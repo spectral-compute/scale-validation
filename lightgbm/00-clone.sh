e8830273c75a8e1a7eb1d905715189fcc16ffa8f
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:27:01 +0100

    lightgbm

--- lightgbm/00-clone.sh
+++ lightgbm/00-clone.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-set -ETeuo pipefail
-source "$(dirname "$0")"/../util/git.sh
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 do_clone lightgbm https://github.com/microsoft/LightGBM.git v4.6.0
