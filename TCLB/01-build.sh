c8a02a2f319bb3e611e00e25c5705b88ea1a2d12
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 15:09:39 +0100

    tclb

--- TCLB/01-build.sh
+++ TCLB/01-build.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-set -ETeuo pipefail
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
 cd TCLB
 ./tools/install.sh rdep
 make configure
 ./configure --with-cuda-arch="${CUDAARCHS}"
-make d2q9 -j$(nproc)
+make d2q9 -j"$(nproc)"
