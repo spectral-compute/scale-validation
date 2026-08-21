1c4ae1ea542e1b27bd2f6140d831da96a8e317e3
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 09:55:04 +0100

    colmap

--- colmap/01-configure.sh
+++ colmap/01-configure.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -euo pipefail
+args=(
+    -GNinja
+    -DCMAKE_BUILD_TYPE=Release
+    -DBLA_VENDOR="Intel10_64lp"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCMAKE_CUDA_COMPILER="nvcc"
+    -DCUDA_ENABLED="true"
+    -DDOWNLOAD_ENABLED="false"
+    -DUNINSTALL_ENABLED="false"
+    -DTESTS_ENABLED="true"
+)
 
 cmake \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBLA_VENDOR="Intel10_64lp" \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCMAKE_CUDA_COMPILER="nvcc" \
-    -DCUDA_ENABLED="true" \
-    -DDOWNLOAD_ENABLED="false" \
-    -DUNINSTALL_ENABLED="false" \
-    -DTESTS_ENABLED="true" \
-    "colmap"
+    "${args[@]}" \
+    -S"colmap" \
+    -B"build"
