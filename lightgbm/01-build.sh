f6f58ff4787bd4d7fc8585f9adfa555eea2c4947
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-21 14:27:01 +0100

    lightgbm

--- lightgbm/01-build.sh
+++ lightgbm/01-build.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+. "$(dirname "$0")"/../util/prelude.sh
 
-set -ETeuo pipefail
+args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_C_COMPILER="clang"
+    -DCMAKE_CXX_COMPILER="clang++"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
+    -DCMAKE_INSTALL_PREFIX="install"
 
-# Configure.
+    -DUSE_GPU=ON
+    -DUSE_CUDA=ON
+    -DBUILD_CPP_TEST=ON
+)
+
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    -DUSE_GPU=ON \
-    -DUSE_CUDA=ON \
-    -DBUILD_CPP_TEST=ON \
-    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
-    -DCMAKE_INSTALL_PREFIX="install" \
+    "${args[@]}" \
     -B"build" \
     "lightgbm"
 
