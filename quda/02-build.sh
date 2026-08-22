a17d77abf10b8d517bf4aa64ee608b87bac2494c
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2026-08-22 17:28:24 +0100

    quda: Switch C++ compiler

--- quda/02-build.sh
+++ quda/02-build.sh
@@ -2,6 +2,14 @@
 
 set -ETeuo pipefail
 
+EXTRA_CMAKE_ARGS=()
+if [ "$(basename "$(realpath "$(which nvcc)")")" == "clang" ] ; then
+    EXTRA_CMAKE_ARGS+=(
+        "-DCMAKE_C_COMPILER=$(realpath "$(which nvcc)")"
+        "-DCMAKE_CXX_COMPILER=$(realpath "$(which nvcc)")++"
+    )
+fi
+
 # Configure.
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
@@ -9,6 +17,7 @@ cmake \
     -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}" \
     -DQUDA_GPU_ARCH="${CUDAARCHS}" \
     -DCMAKE_INSTALL_PREFIX="install" \
+    "${EXTRA_CMAKE_ARGS[@]}" \
     -B"build" \
     "quda"
 
