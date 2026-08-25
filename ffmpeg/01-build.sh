9db6a26425b66b403370e24fb2b46309d89e2634
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-25 16:22:09 +0100

    S ffmpeg

--- ffmpeg/01-build.sh
+++ ffmpeg/01-build.sh
@@ -9,14 +9,14 @@ make -C nv-codec-headers PREFIX="${INSTALL_PREFIX}" install
 export PKG_CONFIG_PATH="${INSTALL_PREFIX}/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 
 # Configure with CUDA/NVENC/NVDEC support.
-cd ffmpeg &&
+(cd ffmpeg &&
     ./configure \
         --prefix="${INSTALL_PREFIX}" \
         --enable-nonfree \
         --disable-x86asm \
         --enable-filter=scale_cuda \
         --enable-cuda-nvcc \
-        --nvccflags="-gencode arch=compute_$CUDAARCHS,code=sm_$CUDAARCHS -O2"
+        --nvccflags="-gencode arch=compute_$CUDAARCHS,code=sm_$CUDAARCHS -O2")
 
 make -O -C ffmpeg -j"$(nproc)"
 make -O -C ffmpeg -j"$(nproc)" install
