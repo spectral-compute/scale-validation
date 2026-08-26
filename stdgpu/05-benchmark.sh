ed0cbade091129940b7b68cc1fffc36e14f18440
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-26 11:05:27 +0100

    S stdgpu bash is hard

--- stdgpu/05-benchmark.sh
+++ stdgpu/05-benchmark.sh
@@ -2,10 +2,10 @@
 . "$(dirname "$0")"/../util/prelude.sh
 
 # Skip faulty benchmarks. See scale#385.
-SKIP="stdgpu_unordered_.*_insert/.*
-stdgpu_unordered_.*_erase/.*
-stdgpu_unordered_.*_clear/.*"
+SKIP=(
+    "stdgpu_unordered_.*_insert/.*"
+    "stdgpu_unordered_.*_erase/.*"
+    "stdgpu_unordered_.*_clear/.*"
+)
 
-SKIP="${SKIP// /:}"
-
-./build/bin/benchmarkstdgpu --benchmark_filter="-$SKIP" --benchmark_out_format=csv --benchmark_out=out.csv --v=3
+./build/bin/benchmarkstdgpu --benchmark_filter="-$(IFS=':' ; echo "${SKIP[*]}")" --benchmark_out_format=csv --benchmark_out=out.csv --v=3
