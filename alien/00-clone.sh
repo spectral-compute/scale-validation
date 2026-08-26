fad34fdab7e88e7f571bd92f5d34d1e4e43adfa7
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-26 10:41:44 +0100

    S alien vcpkg

--- alien/00-clone.sh
+++ alien/00-clone.sh
@@ -2,3 +2,6 @@
 . "$(dirname "$0")"/../util/prelude.sh
 
 do_clone alien https://github.com/chrxh/alien.git "$(get_version alien)"
+
+# do_clone uses --shallow-submodules by default to save space, but this makes vcpkg unhappy
+git -C alien/external/vcpkg fetch --unshallow
