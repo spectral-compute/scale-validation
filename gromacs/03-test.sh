d5ab915f69af1d3c9e469d9075bafd045e6814ce
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-08-26 15:44:55 +0100

    S gromacs error handling

--- gromacs/03-test.sh
+++ gromacs/03-test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
 
-. "install/bin/GMXRC"
+# GMXRC doesn't like the strict error flags we use in our scripts
+set +ETeuo pipefail
+source "install/bin/GMXRC"
+set -ETeuo pipefail
+
 make -O -C build tests -j
 ctest --test-dir build --verbose 2>&1 | tee "regression-test.txt"
