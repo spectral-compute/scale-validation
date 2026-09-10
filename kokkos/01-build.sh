e9b16774e801d54d1fad8f07dd3ee5d6d30f63ab
Chris Kitching <chris@spectralcompute.com>
2026-09-10 20:53:15 +0200

    Explicitly enable warpsize emulation for kokkos
    Since the current build script uses clang mode, it exposes the
    native warpsize by default, but kokkos' source code is not actually
    written to be ws-agnostic.

--- kokkos/01-build.sh
+++ kokkos/01-build.sh
@@ -10,6 +10,9 @@ cmake \
     -DKokkos_ENABLE_CUDA=ON \
     -DKokkos_ARCH_AMPERE86=ON \
     -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_CUDA_FLAGS="-DSCALE_WARP_SIZE=32" \
+    -DCMAKE_CXX_FLAGS="-DSCALE_WARP_SIZE=32" \
+    -DCMAKE_C_FLAGS="-DSCALE_WARP_SIZE=32" \
     -DKokkos_ENABLE_TESTS=ON \
     -B"build" \
     "kokkos"
