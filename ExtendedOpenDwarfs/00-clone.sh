cccca1379edebe05c3cbde6f94c3a5d681deaa6a
Aria Shrimpton <aria@spectralcompute.co.uk>
2026-09-18 15:55:39 +0100

    TMP use my fork of extendedopendwarfs

--- ExtendedOpenDwarfs/00-clone.sh
+++ ExtendedOpenDwarfs/00-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
 
-do_clone ExtendedOpenDwarfs https://github.com/ANU-HPC/ExtendedOpenDwarfs.git "$(get_version ExtendedOpenDwarfs)"
+do_clone ExtendedOpenDwarfs https://github.com/tcmal/ExtendedOpenDwarfs.git aria/cleanup
