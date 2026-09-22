6eb07be8a808f757870a1babc75c795cf5281c3b
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:32:26 +0100

    rodinia_suite

--- rodinia_suite/00-clone.sh
+++ rodinia_suite/00-clone.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # TODO: Figure out why we have a fork, and not do that
 do_clone rodinia_suite https://github.com/manospavlidakis/rodinia_suite.git "$(get_version rodinia_suite)"
--- rodinia_suite/01-build.sh
+++ rodinia_suite/01-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 ./rodinia_suite/cuda/buildall.sh --cuda "${CUDA_PATH}" --sm "${CUDAARCHS}" --spectral
