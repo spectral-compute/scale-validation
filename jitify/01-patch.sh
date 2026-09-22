512ea2f47b132f4696718b482c122bc7814dc8ed
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:33:43 +0100

    jitify

--- jitify/01-patch.sh
+++ jitify/01-patch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # Ensure we build google test with -fPIE, to avoid a link error
 # shellcheck disable=SC2016
--- jitify/02-build.sh
+++ jitify/02-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 make -O -C jitify jitify_test NVCC="$(which nvcc)"
