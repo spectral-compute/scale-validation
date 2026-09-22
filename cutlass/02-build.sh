8e5142e8af058ce5a348e4f4c752e9f4d9b26286
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    cutlass

--- cutlass/02-build.sh
+++ cutlass/02-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # We don't support the warpgroup stuff yet.
 export SCALE_CUDA_VERSION="11.8"
--- cutlass/03-test.sh
+++ cutlass/03-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 LOGFILE="tests.log"
 log "Writing to $LOGFILE"
