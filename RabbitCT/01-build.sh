39a383ebda2105933b9be80046c849c869474a21
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:50 +0100

    RabbitCT

--- RabbitCT/01-build.sh
+++ RabbitCT/01-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # Copied from their mk/config-default.mk but need a few overriding
 # options to stop a few build errors
