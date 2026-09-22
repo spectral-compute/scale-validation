981701f11fc0044dba18524b3b55f20c1429d565
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:37:14 +0100

    ggml

--- ggml/00-clone.sh
+++ ggml/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone ggml https://github.com/ggml-org/ggml "$(get_version ggml)"
--- ggml/02-run-yolo.sh
+++ ggml/02-run-yolo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 YOLO_UTILS_DIR="$(realpath ./yolo_utils)"
 mkdir -p "$YOLO_UTILS_DIR"
