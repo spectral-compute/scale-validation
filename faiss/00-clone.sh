b460770bd84149098cc15f47a421a69e9ef40dd1
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    faiss

--- faiss/00-clone.sh
+++ faiss/00-clone.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone faiss https://github.com/facebookresearch/faiss.git "$(get_version faiss)"
 
--- faiss/01-patch.sh
+++ faiss/01-patch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 for P in "${SCRIPT_DIR}"/*.patch; do
     (cd faiss && patch -p0 <"${P}")
