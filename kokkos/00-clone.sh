f66db57e3ed7ff711bf825fec07d4cb1429312ab
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:33:33 +0100

    kokkos

--- kokkos/00-clone.sh
+++ kokkos/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone kokkos https://github.com/kokkos/kokkos.git "$(get_version kokkos)"
--- kokkos/02-test.sh
+++ kokkos/02-test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 ctest --test-dir build/ -parallel 8 --verbose
