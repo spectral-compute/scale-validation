461fa3b19eba4fcc8da159268a0a43317846a313
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    cuml

--- cuml/02-test.sh
+++ cuml/02-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 ./cuml/cpp/build/test/ml    # Single GPU algorithm tests
 ./cuml/cpp/build/test/ml_mg # Multi GPU algorithm tests, if --singlegpu was not used
