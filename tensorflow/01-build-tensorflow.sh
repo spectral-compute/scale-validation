9999a055dc9933c62452d2d902734b2016852e63
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:31:58 +0100

    tensorflow

--- tensorflow/01-build-tensorflow.sh
+++ tensorflow/01-build-tensorflow.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # Clear the bazel cache so we always get a clean build.
 rm -rf ~/.cache/bazel
--- tensorflow/03-mnist-benchmark.sh
+++ tensorflow/03-mnist-benchmark.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # Create somewhere for results.
 mkdir -p "${OUT_DIR}/tensorflow/benchmarks"
