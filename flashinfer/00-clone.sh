cca32fc022f1ac9ec1420d834d37a7ac8e5e52cc
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    flashinfer

--- flashinfer/00-clone.sh
+++ flashinfer/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone flashinfer https://github.com/flashinfer-ai/flashinfer.git "$(get_version flashinfer)"
--- flashinfer/01-build.sh
+++ flashinfer/01-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 export SCALE_CUDA_VERSION="12.1"
 
