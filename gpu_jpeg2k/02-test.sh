7c360a76ef590fc22dcaf344ade66d4746ab197b
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    gpu_jpeg2k

--- gpu_jpeg2k/02-test.sh
+++ gpu_jpeg2k/02-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 if ! [ -f BSDS300-images.tgz ]; then
     # Some images, and the result of running them through this on nvidia.
