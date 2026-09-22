1e36e017a1ea8520f7bca90cf4ab83896d6a0bc2
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    FLAMEGPU2

--- FLAMEGPU2/00-clone.sh
+++ FLAMEGPU2/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone FLAMEGPU2 https://github.com/FLAMEGPU/FLAMEGPU2.git "$(get_version FLAMEGPU2)"
--- FLAMEGPU2/03-test.sh
+++ FLAMEGPU2/03-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # Err why is this needed only in CI?
 export CUDA_PATH
