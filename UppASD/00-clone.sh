5ed185c0f862611b72f327f71fd68dc9d3aa6352
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:50 +0100

    UppASD

--- UppASD/00-clone.sh
+++ UppASD/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone UppASD https://github.com/UppASD/UppASD.git "$(get_version UppASD)"
--- UppASD/01-patch.sh
+++ UppASD/01-patch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 # If building on a different machine than running on, you can set NO_TUNE_NATIVE=1 to
 # avoid SIGILL errors.
