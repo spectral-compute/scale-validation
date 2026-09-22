f978e7748be5ee6947a888d19a2a33f55d7e7ebf
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:34:01 +0100

    opencv

--- opencv/00-clone.sh
+++ opencv/00-clone.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone opencv https://github.com/opencv/opencv.git "$(get_version opencv)"
 do_clone opencv_contrib https://github.com/opencv/opencv_contrib.git "$(get_version opencv_contrib)"
