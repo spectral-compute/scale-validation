aff94a1dfef9dae53de4ac205b90faec9d65e898
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    ffmpeg

--- ffmpeg/00-clone.sh
+++ ffmpeg/00-clone.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone nv-codec-headers https://github.com/FFmpeg/nv-codec-headers.git "$(get_version nv-codec-headers)"
 do_clone ffmpeg https://github.com/FFmpeg/FFmpeg.git "$(get_version ffmpeg)"
