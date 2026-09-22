eab5520a464bf2b9aca5a3d300d7e2e248d363e1
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:29:21 +0100

    AMGX

--- AMGX/00-clone.sh
+++ AMGX/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone AMGX https://github.com/NVIDIA/AMGX.git "$(get_version AMGX)"
--- AMGX/02-build.sh
+++ AMGX/02-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 OUT_DIR="$(realpath ../)"
 if [ ! -e "${OUT_DIR}/openmpi/install" ]; then
