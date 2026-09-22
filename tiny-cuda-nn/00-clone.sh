05e905fe75f2df3db48f235cef97ca4f54d5d182
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:50 +0100

    tiny-cuda-nn

--- tiny-cuda-nn/00-clone.sh
+++ tiny-cuda-nn/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone tiny-cuda-nn https://github.com/NVlabs/tiny-cuda-nn "$(get_version tiny-cuda-nn)"
--- tiny-cuda-nn/02-examples.sh
+++ tiny-cuda-nn/02-examples.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 ./build/mlp_learning_an_image tiny-cuda-nn/data/images/albert.jpg 10000
