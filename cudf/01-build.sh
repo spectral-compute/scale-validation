b5aa89cba9ec307d780e85bd563f1aa478bb015c
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    cudf

--- cudf/01-build.sh
+++ cudf/01-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 (cd cudf && ./build.sh --pydevelop libcudf libcudf_kafka cudf dask_cudf cudf_kafka custreamz)
