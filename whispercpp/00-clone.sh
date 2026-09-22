ee61afa2720a2cf14dfd716dc4726870db44a8fd
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:30:53 +0100

    whispercpp

--- whispercpp/00-clone.sh
+++ whispercpp/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone whispercpp https://github.com/ggerganov/whisper.cpp.git "$(get_version whispercpp)"
