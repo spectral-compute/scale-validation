7fb84d728a6aa4fe47191f46a746a93a4095014e
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:31:46 +0100

    timemachine

--- timemachine/00-clone.sh
+++ timemachine/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone timemachine https://github.com/proteneer/timemachine.git "$(get_version timemachine)"
