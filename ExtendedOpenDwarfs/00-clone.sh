ca68d291d3b93564ea41b79c85abe9a798f98dba
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    ExtendedOpenDwarfs

--- ExtendedOpenDwarfs/00-clone.sh
+++ ExtendedOpenDwarfs/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone ExtendedOpenDwarfs https://github.com/ANU-HPC/ExtendedOpenDwarfs.git "$(get_version ExtendedOpenDwarfs)"
