2e2c15a8be2b8cc540ab1bb4e964bc52a3efa312
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:36:50 +0100

    hypre

--- hypre/01-build.sh
+++ hypre/01-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 MPI_DIR="$(realpath ../)/openmpi/install"
 if [ ! -e "${MPI_DIR}" ]; then
--- hypre/02-test.sh
+++ hypre/02-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 for T in bench error fac gpumemcheck ij lobpcg longdouble single sstruct struct superlu timing; do
     (cd "build/src/test" && ./runtest.sh -t TEST_${T}/*.sh)
