a7a1936517714bfc60aba11cb1aaf6dc5346b176
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    ds4

--- ds4/00-clone.sh
+++ ds4/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone ds4 https://github.com/antirez/ds4.git "$(get_version ds4)"
--- ds4/01-build.sh
+++ ds4/01-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 (cd ds4 && make cuda-generic && ./download_model.sh q2-imatrix)
