5760de40931514f25c17f417e0e952e7e796f063
Andrew Lamzed-Short <andrew@spectralcompute.co.uk>
2026-09-22 15:39:49 +0100

    cuda-samples

--- cuda-samples/00-clone.sh
+++ cuda-samples/00-clone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
+raise_error_if_using_rocm
 
 do_clone cuda-samples https://github.com/NVIDIA/cuda-samples.git "$(get_version cuda-samples)"
