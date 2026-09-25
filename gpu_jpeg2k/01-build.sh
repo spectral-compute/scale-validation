1c9d52f7c6a3e4532216caf795f63747d4457558
Chris Kitching <chris@spectralcompute.com>
2026-09-25 19:11:27 +0200

    gpu_jpeg2k: use CUDAARCHS directly

--- gpu_jpeg2k/01-build.sh
+++ gpu_jpeg2k/01-build.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 . "$(dirname "$0")"/../util/prelude.sh
 
-SM_NUM="${CUDAARCHS}"
-
 # This project is pretty ancient and has its own rather loopy way to get flags into the
 # nvcc call. CMake list syntax is required in CUDA_NVCC_FLAGS_RELEASE.
 args=(
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5
     -DCMAKE_BUILD_TYPE=Release
-    -DCMAKE_CUDA_ARCHITECTURES="${SM_NUM}"
+    -DCMAKE_CUDA_ARCHITECTURES="${CUDAARCHS}"
 
     # Work around some overly strict compiler/cuda toolkit detection
     "-DCMAKE_C_COMPILER=$(which clang)"
@@ -18,7 +16,7 @@ args=(
 
     -DCMAKE_C_FLAGS="-Wno-error=implicit-function-declaration -Wno-error=implicit-int -Wno-deprecated-declarations"
     -DCMAKE_CXX_FLAGS="-Wno-unused-result -Wno-stringop-overread -Wno-switch -Wno-deprecated-declaration"
-    -DCUDA_NVCC_FLAGS_RELEASE="-gencode;arch=compute_${SM_NUM},code=sm_${SM_NUM};-Wno-unused-result;-Wno-switch;-Wno-error=implicit-const-int-float-conversion;-Wno-deprecated-declarations"
+    -DCUDA_NVCC_FLAGS_RELEASE="-gencode;arch=compute_${CUDAARCHS},code=sm_${CUDAARCHS};-Wno-unused-result;-Wno-switch;-Wno-error=implicit-const-int-float-conversion;-Wno-deprecated-declarations"
 )
 
 cmake \
